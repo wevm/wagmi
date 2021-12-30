4b6b452354a978e52a05901a996d9e657c075dc3
Tom Meagher <tom@meagher.co>
2021-12-30 11:19:05 -0500

    docs: add stork search

--- examples/vite-react/src/App.tsx
+++ examples/vite-react/src/App.tsx
@@ -1,77 +1,23 @@
-import { useAccount, useConnect, useNetwork } from 'wagmi'
+import { useAccount } from 'wagmi'
 
-export const App = () => {
-  const [{ data: accountData }, disconnect] = useAccount({
-    fetchBalance: true,
-    fetchEns: true,
-  })
-  const [{ data: networkData, error: switchNetworkError }, switchNetwork] =
-    useNetwork()
+import { Account, Connect, NetworkSwitcher } from './components'
 
-  const [
-    {
-      data: { connector, connectors },
-      error,
-      loading,
-    },
-    connect,
-  ] = useConnect()
+const Page = () => {
+  const [{ data: accountData }] = useAccount()
 
   if (accountData?.address)
     return (
       <main>
-        <div>
-          <button onClick={() => disconnect()}>
-            Disconnect from {accountData?.connector?.name}
-          </button>
-        </div>
-
-        <div>
-          {accountData?.ens?.name ?? accountData?.address}
-          {accountData?.ens ? ` (${accountData?.address})` : null}
-        </div>
-        {accountData?.ens?.avatar ? (
-          <img src={accountData.ens.avatar} style={{ height: 40, width: 40 }} />
-        ) : (
-          <div style={{ height: 40, width: 40 }} />
-        )}
-
-        <div>
-          <div>
-            {networkData.chain?.name ?? networkData.chain?.id}{' '}
-            {networkData.chain?.unsupported && '(unsupported)'}
-          </div>
-
-          <div>
-            Balance: {accountData?.balance?.formatted}{' '}
-            {accountData?.balance?.symbol}{' '}
-          </div>
-
-          {switchNetwork &&
-            networkData.chains.map((x) =>
-              x.id === networkData.chain?.id ? null : (
-                <button key={x.id} onClick={() => switchNetwork(x.id)}>
-                  Switch to {x.name}
-                </button>
-              ),
-            )}
-
-          {switchNetworkError && switchNetworkError?.message}
-        </div>
+        <Account />
+        <NetworkSwitcher />
       </main>
     )
 
   return (
     <main>
-      <div>
-        {connectors.map((x) => (
-          <button key={x.name} onClick={() => connect(x)}>
-            {x.name}
-            {loading && x.name === connector?.name && '…'}
-          </button>
-        ))}
-      </div>
-      <div>{error && (error?.message ?? 'Failed to connect')}</div>
+      <Connect />
     </main>
   )
 }
+
+export default Page
---
+++ examples/vite-react/src/components/Connect.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react'
+import { useConnect } from 'wagmi'
+
+export const Connect = () => {
+  const [
+    {
+      data: { connector, connectors },
+      error,
+      loading,
+    },
+    connect,
+  ] = useConnect()
+
+  return (
+    <>
+      <div>
+        {connectors.map((x) => (
+          <button key={x.name} onClick={() => connect(x)}>
+            {x.name}
+            {loading && x.name === connector?.name && '…'}
+          </button>
+        ))}
+      </div>
+      <div>{error && (error?.message ?? 'Failed to connect')}</div>
+    </>
+  )
+}
