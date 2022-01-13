8e1dee9d0ca69b53c43c30794d73dfe86b81aa6c
Sam Margalit <sam@margalit.com.au>
2022-01-13 21:38:52 +1100

    docs: copy components into remix example

--- examples/remix/app/routes/index.tsx
+++ examples/remix/app/routes/index.tsx
@@ -1,32 +1,17 @@
+import { useAccount } from 'wagmi'
+
+import { Account, Connect, NetworkSwitcher } from '../components'
+
 export default function Index() {
-  return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  )
+  const [{ data: accountData }] = useAccount()
+
+  if (accountData?.address)
+    return (
+      <>
+        <Account />
+        <NetworkSwitcher />
+      </>
+    )
+
+  return <Connect />
 }
