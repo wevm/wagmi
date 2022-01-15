9fc66225ddab33f1ecf40e7231d31a360d2c258c
Sam Margalit <sam@margalit.com.au>
2022-01-16 09:30:06 +1100

    docs: add dotenv to remix example

--- examples/remix/app/root.tsx
+++ examples/remix/app/root.tsx
@@ -6,6 +6,7 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from 'remix'
 import type { MetaFunction } from 'remix'
 
@@ -15,64 +16,67 @@ import { InjectedConnector } from 'wagmi/connectors/injected'
 import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
 import { WalletLinkConnector } from 'wagmi/connectors/walletLink'
 
-// Get environment variables
-const alchemy = process.env.REMIX_ALCHEMY_ID as string
-const etherscan = process.env.REMIX_ETHERSCAN_API_KEY as string
-const infuraId = process.env.REMIX_INFURA_ID as string
+export function loader() {
+  require('dotenv').config()
+  return {
+    alchemy: process.env.REMIX_ALCHEMY_ID as string,
+    etherscan: process.env.REMIX_ETHERSCAN_API_KEY as string,
+    infuraId: process.env.REMIX_INFURA_ID as string,
+  }
+}
 
-// Pick chains
-const chains = defaultChains
-const defaultChain = chain.mainnet
+function WagmiProvider({ children }) {
+  // Get environment variables
+  const { alchemy, etherscan, infuraId } = useLoaderData()
 
-// Set up connectors
-type ConnectorsConfig = { chainId?: number }
-const connectors = ({ chainId }: ConnectorsConfig) => {
-  const rpcUrl =
-    chains.find((x) => x.id === chainId)?.rpcUrls?.[0] ??
-    defaultChain.rpcUrls[0]
-  return [
-    new InjectedConnector({ chains }),
-    new WalletConnectConnector({
-      chains,
-      options: {
+  // Pick chains
+  const chains = defaultChains
+  const defaultChain = chain.mainnet
+
+  // Set up connectors
+  type ConnectorsConfig = { chainId?: number }
+  const connectors = ({ chainId }: ConnectorsConfig) => {
+    const rpcUrl =
+      chains.find((x) => x.id === chainId)?.rpcUrls?.[0] ??
+      defaultChain.rpcUrls[0]
+    return [
+      new InjectedConnector({ chains }),
+      // new WalletConnectConnector({
+      //   chains,
+      //   options: {
+      //     infuraId,
+      //     qrcode: true,
+      //   },
+      // }),
+      // new WalletLinkConnector({
+      //   chains,
+      //   options: {
+      //     appName: 'wagmi',
+      //     jsonRpcUrl: `${rpcUrl}/${infuraId}`,
+      //   },
+      // }),
+    ]
+  }
+
+  // Set up providers
+  type ProviderConfig = { chainId?: number; connector?: Connector }
+  const isChainSupported = (chainId?: number) =>
+    chains.some((x) => x.id === chainId)
+
+  const provider = ({ chainId }: ProviderConfig) =>
+    providers.getDefaultProvider(
+      isChainSupported(chainId) ? chainId : defaultChain.id,
+      {
+        alchemy,
+        etherscan,
         infuraId,
-        qrcode: true,
       },
-    }),
-    new WalletLinkConnector({
-      chains,
-      options: {
-        appName: 'wagmi',
-        jsonRpcUrl: `${rpcUrl}/${infuraId}`,
-      },
-    }),
-  ]
-}
+    )
+  const webSocketProvider = ({ chainId }: ProviderConfig) =>
+    isChainSupported(chainId)
+      ? new providers.InfuraWebSocketProvider(chainId, infuraId)
+      : undefined
 
-// Set up providers
-type ProviderConfig = { chainId?: number; connector?: Connector }
-const isChainSupported = (chainId?: number) =>
-  chains.some((x) => x.id === chainId)
-
-const provider = ({ chainId }: ProviderConfig) =>
-  providers.getDefaultProvider(
-    isChainSupported(chainId) ? chainId : defaultChain.id,
-    {
-      alchemy,
-      etherscan,
-      infuraId,
-    },
-  )
-const webSocketProvider = ({ chainId }: ProviderConfig) =>
-  isChainSupported(chainId)
-    ? new providers.InfuraWebSocketProvider(chainId, infuraId)
-    : undefined
-
-export const meta: MetaFunction = () => {
-  return { title: 'wagmi' }
-}
-
-export default function App() {
   return (
     <Provider
       autoConnect
@@ -80,20 +84,32 @@ export default function App() {
       provider={provider}
       webSocketProvider={webSocketProvider}
     >
-      <html lang="en">
-        <head>
-          <meta charSet="utf-8" />
-          <meta name="viewport" content="width=device-width,initial-scale=1" />
-          <Meta />
-          <Links />
-        </head>
-        <body>
-          <Outlet />
-          <ScrollRestoration />
-          <Scripts />
-          {process.env.NODE_ENV === 'development' && <LiveReload />}
-        </body>
-      </html>
+      {children}
     </Provider>
+  )
+}
+
+export const meta: MetaFunction = () => {
+  return { title: 'wagmi' }
+}
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <WagmiProvider>
+          <Outlet />
+        </WagmiProvider>
+        <ScrollRestoration />
+        <Scripts />
+        {process.env.NODE_ENV === 'development' && <LiveReload />}
+      </body>
+    </html>
   )
 }
