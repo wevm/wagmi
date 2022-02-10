b96d79b81926373434f02aed6e8041e732f1ad27
Tom Meagher <tom@meagher.co>
2022-02-09 19:06:00 -0500

    docs: bump nextra, i18n support

--- docs/next.config.js
+++ docs/next.config.js
@@ -2,11 +2,16 @@ const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.js',
   unstable_stork: true,
+  unstable_contentDump: true,
   unstable_staticImage: true,
 })
 
 /** @type {import('next').NextConfig} */
 const config = {
+  i18n: {
+    locales: ['en-US'],
+    defaultLocale: 'en-US',
+  },
   reactStrictMode: true,
 }
 
--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -1,10 +1,7 @@
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,md,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,md,mdx}',
+    './theme.config.js',
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
 }
