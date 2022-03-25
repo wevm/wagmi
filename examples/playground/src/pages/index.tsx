3c6e8722f588055bfaf6eb051eb69adb792f28ab
moxey.eth <jakemoxey@gmail.com>
2022-03-25 13:28:40 +1100

    remove code snippet from playground

--- examples/playground/src/pages/index.tsx
+++ examples/playground/src/pages/index.tsx
@@ -1,24 +1,10 @@
 import * as React from 'react'
-import { useConnect } from 'wagmi'
 
 import { Account, Connect, NetworkSwitcher } from '../components'
 import { useIsMounted } from '../hooks'
 
 const Page = () => {
   const isMounted = useIsMounted()
-
-  const { connectors, connectAsync } = useConnect()
-
-  React.useEffect(() => {
-    ;(async () => {
-      connectAsync(connectors[0])
-
-      connectors[0].on('connecting', async () => {
-        const provider = await connectors[0].getProvider()
-        console.log('test2', provider)
-      })
-    })()
-  }, [connectAsync, connectors])
 
   if (!isMounted) return null
   return (
