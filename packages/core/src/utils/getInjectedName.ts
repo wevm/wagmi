e00abd34d05805dd72cd15ac6e26719c674a7baf
Tom Meagher <tom@meagher.co>
2022-04-05 18:59:03 -0400

    refactor: window ethereum types

--- packages/core/src/utils/getInjectedName.ts
+++ packages/core/src/utils/getInjectedName.ts
@@ -1,6 +1,7 @@
 export function getInjectedName(ethereum?: Window['ethereum']) {
   if (!ethereum) return 'Injected'
   if (ethereum.isMetaMask) return 'MetaMask'
+  if (ethereum.isBraveWallet) return 'Brave Wallet'
   if (ethereum.isCoinbaseWallet) return 'Coinbase Wallet'
   if (ethereum.isFrame) return 'Frame'
   if (ethereum.isTally) return 'Tally'
