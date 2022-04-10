cf07b996d0cc6c32d3d649c1a8db0d56a5eaae05
awkweb <tom@meagher.co>
2022-04-10 19:17:02 -0400

    test: useChainId (#330)

--- packages/react/src/hooks/utils/useChainId.test.ts
+++ packages/react/src/hooks/utils/useChainId.test.ts
@@ -6,4 +6,17 @@ describe('useChainId', () => {
     const { result } = renderHook(() => useChainId())
     expect(result.current).toMatchInlineSnapshot(`31337`)
   })
+
+  describe.each`
+    chainId      | expected
+    ${undefined} | ${31337}
+    ${1}         | ${1}
+    ${4}         | ${4}
+    ${12345}     | ${31337}
+  `('useChainId({ chainId: $chainId })', ({ chainId, expected }) => {
+    it(`returns ${expected}`, () => {
+      const { result } = renderHook(() => useChainId({ chainId }))
+      expect(result.current).toEqual(expected)
+    })
+  })
 })
