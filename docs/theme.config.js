2df6f8d18e03e8707d99c3ce19307414d04e843f
Tom Meagher <tom@meagher.co>
2022-04-25 09:51:21 -0400

    docs: update meta

--- docs/theme.config.js
+++ docs/theme.config.js
@@ -30,27 +30,24 @@ export default {
   head: ({ title, meta }) => {
     // eslint-disable-next-line react-hooks/rules-of-hooks
     const { systemTheme } = useTheme()
+    const description_ =
+      meta.description_ ||
+      'wagmi is a collection of React Hooks containing everything you need to start working with Ethereum. wagmi makes it easy to "Connect Wallet," display ENS and balance information, sign messages, interact with contracts, and much more — all with caching, request deduplication, and persistence.'
+    const title_ =
+      title && !title.startsWith('wagmi')
+        ? title + ' – wagmi'
+        : 'wagmi: React Hooks for Ethereum'
     return (
       <>
         {/* General */}
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta httpEquiv="Content-Language" content="en" />
+        <title>{title_}</title>
 
         {/* SEO */}
-        <meta
-          name="description"
-          content={meta.description || 'React Hooks for Ethereum.'}
-        />
-        <meta
-          name="og:description"
-          content={meta.description || 'React Hooks for Ethereum.'}
-        />
-        <meta
-          name="og:title"
-          content={
-            title ? title + ' – wagmi' : 'wagmi: React Hooks for Ethereum'
-          }
-        />
+        <meta name="description" content={description_} />
+        <meta name="og:description" content={description_} />
+        <meta name="og:title" content={title_} />
         <meta name="og:image" content="https://wagmi.sh/og.png" />
         <meta name="twitter:card" content="summary_large_image" />
 
