e6d9998c83e7289ec3f2e2e6b1f664bc35a9e1cb
moxey.eth <jakemoxey@gmail.com>
2022-06-09 11:09:43 +1000

    fix: fixed parseContractResult for more complex contract types

--- packages/react/src/utils/parseContractResult.test.ts
+++ packages/react/src/utils/parseContractResult.test.ts
@@ -1,8 +1,9 @@
 import { BigNumber } from 'ethers'
+import { Result } from 'ethers/lib/utils'
 
 import { parseContractResult } from './parseContractResult'
 
-const gmContractInterface = [
+const contractInterface = [
   {
     inputs: [
       {
@@ -27,42 +28,401 @@ const gmContractInterface = [
     stateMutability: 'view',
     type: 'function',
   },
-]
-
-const wagmigotchiContractConfig = [
   {
-    inputs: [{ internalType: 'address', name: '', type: 'address' }],
-    name: 'love',
-    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    name: 'withoutNames',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    name: 'simpleTuples',
+    outputs: [
+      {
+        components: [
+          {
+            internalType: 'uint256',
+            name: 'timestamp',
+            type: 'uint256',
+          },
+          {
+            internalType: 'address',
+            name: 'sender',
+            type: 'address',
+          },
+        ],
+        internalType: 'struct Gms[]',
+        name: '',
+        type: 'tuple[]',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    name: 'complexTuples',
+    outputs: [
+      {
+        components: [
+          {
+            internalType: 'uint256',
+            name: 'timestamp',
+            type: 'uint256',
+          },
+          {
+            internalType: 'address',
+            name: 'sender',
+            type: 'address',
+          },
+          {
+            components: [
+              {
+                internalType: 'uint256',
+                name: 'timestamp',
+                type: 'uint256',
+              },
+              {
+                internalType: 'address',
+                name: 'sender',
+                type: 'address',
+              },
+            ],
+            internalType: 'struct Gms[]',
+            name: 'gms',
+            type: 'tuple[]',
+          },
+        ],
+        internalType: 'struct Gms[]',
+        name: '',
+        type: 'tuple[]',
+      },
+    ],
     stateMutability: 'view',
     type: 'function',
   },
 ]
 
 describe('parseContractResult', () => {
-  it('should parse the data to an ethers Result if there are no named keys', () => {
-    const data = [
-      BigNumber.from(1654322661),
-      '0xa5cc3c03994DB5b0d9A5eEdD10CabaB0813678AC',
-    ]
-    expect(
-      parseContractResult({
-        contractInterface: gmContractInterface,
+  describe('gms', () => {
+    it('should parse the data to an ethers Result if there are no named keys', () => {
+      const data = [
+        BigNumber.from(1654322661),
+        '0xa5cc3c03994DB5b0d9A5eEdD10CabaB0813678AC',
+      ]
+      const result = parseContractResult({
+        contractInterface,
         data,
-
         functionName: 'gms',
-      }),
-    ).toEqual(Object.assign(data, { timestamp: data[0], sender: data[1] }))
+      })
+      expect(result).toEqual(data)
+      expect(Object.keys(result).length !== data.length).toBeTruthy()
+      expect(result.timestamp).toEqual(data[0])
+      expect(result.sender).toEqual(data[1])
+    })
+
+    it('should return the data if in sync with ethers Result', () => {
+      const data = Object.assign(
+        [
+          BigNumber.from(1654322661),
+          '0xa5cc3c03994DB5b0d9A5eEdD10CabaB0813678AC',
+        ],
+        {
+          timestamp: BigNumber.from(1654322661),
+          sender: '0xa5cc3c03994DB5b0d9A5eEdD10CabaB0813678AC',
+        },
+      )
+      const result = parseContractResult({
+        contractInterface,
+        data,
+        functionName: 'gms',
+      })
+      expect(Object.keys(result).length !== data.length).toBeTruthy()
+      expect(result).toEqual(data)
+      expect(result.timestamp).toEqual(data.timestamp)
+      expect(result.sender).toEqual(data.sender)
+    })
   })
 
-  it('should return the data if in sync with ethers Result', () => {
-    const data = [69]
-    expect(
-      parseContractResult({
-        contractInterface: wagmigotchiContractConfig,
+  describe('simpleTuples', () => {
+    it('should parse the data to an ethers Result if there are no named keys', () => {
+      const data: [BigNumber, string][] = [
+        [
+          BigNumber.from(1654322661),
+          '0xa5cc3c03994DB5b0d9A5eEdD10CabaB0813678AC',
+        ],
+        [
+          BigNumber.from(1654322662),
+          '0xa5cc3c03994DB5b0d9A5eEdD10CabaB0813678AD',
+        ],
+      ]
+      const result = parseContractResult({
+        contractInterface,
         data,
-        functionName: 'love',
-      }),
-    ).toEqual(data)
+        functionName: 'simpleTuples',
+      })
+
+      expect(Object.keys(result).length === data.length).toBeTruthy()
+      expect(result[0]).toEqual(data[0])
+      expect(Object.keys(result[0]).length !== data[0]?.length).toBeTruthy()
+      expect(result[0].timestamp).toEqual(data?.[0]?.[0])
+      expect(result[0].sender).toEqual(data?.[0]?.[1])
+      expect(result[1]).toEqual(data[1])
+      expect(Object.keys(result[1]).length !== data[1]?.length).toBeTruthy()
+      expect(result[1].timestamp).toEqual(data[1]?.[0])
+      expect(result[1].sender).toEqual(data[1]?.[1])
+    })
+
+    it('should return the data if in sync with ethers Result', () => {
+      const data: Result = [
+        Object.assign(
+          [
+            BigNumber.from(1654322661),
+            '0xa5cc3c03994DB5b0d9A5eEdD10CabaB0813678AC',
+          ],
+          {
+            timestamp: BigNumber.from(1654322661),
+            sender: '0xa5cc3c03994DB5b0d9A5eEdD10CabaB0813678AC',
+          },
+        ),
+        Object.assign(
+          [
+            BigNumber.from(1654322662),
+            '0xa5cc3c03994DB5b0d9A5eEdD10CabaB0813678AD',
+          ],
+          {
+            timestamp: BigNumber.from(1654322662),
+            sender: '0xa5cc3c03994DB5b0d9A5eEdD10CabaB0813678AD',
+          },
+        ),
+      ]
+      const result = parseContractResult({
+        contractInterface,
+        data,
+        functionName: 'complexTuples',
+      })
+
+      expect(Object.keys(result).length === data.length).toBeTruthy()
+      expect(result[0]).toEqual(data[0])
+      expect(Object.keys(result[0]).length !== data[0]?.length).toBeTruthy()
+      expect(result[0].timestamp).toEqual(data?.[0]?.timestamp)
+      expect(result[0].sender).toEqual(data?.[0]?.sender)
+      expect(result[1]).toEqual(data[1])
+      expect(Object.keys(result[1]).length !== data[1]?.length).toBeTruthy()
+      expect(result[1].timestamp).toEqual(data[1]?.timestamp)
+      expect(result[1].sender).toEqual(data[1]?.sender)
+    })
+  })
+
+  describe('complexTuples', () => {
+    it('should parse the data to an ethers Result if there are no named keys', () => {
+      const data: [BigNumber, string, [BigNumber, string][]][] = [
+        [
+          BigNumber.from(1654322661),
+          '0xa5cc3c03994DB5b0d9A5eEdD10CabaB0813678AC',
+          [
+            [
+              BigNumber.from(1654322661),
+              '0xa5cc3c03994DB5b0d9A5eEdD10CabaB0813678AC',
+            ],
+            [
+              BigNumber.from(1654322661),
+              '0xa5cc3c03994DB5b0d9A5eEdD10CabaB0813678AC',
+            ],
+          ],
+        ],
+        [
+          BigNumber.from(1654322662),
+          '0xa5cc3c03994DB5b0d9A5eEdD10CabaB0813678AD',
+          [
+            [
+              BigNumber.from(1654322661),
+              '0xa5cc3c03994DB5b0d9A5eEdD10CabaB0813678AC',
+            ],
+            [
+              BigNumber.from(1654322661),
+              '0xa5cc3c03994DB5b0d9A5eEdD10CabaB0813678AC',
+            ],
+          ],
+        ],
+      ]
+      const result = parseContractResult({
+        contractInterface,
+        data,
+        functionName: 'complexTuples',
+      })
+
+      expect(Object.keys(result).length === data.length).toBeTruthy()
+      expect(result[0]).toEqual(data[0])
+      expect(Object.keys(result[0]).length !== data[0]?.length).toBeTruthy()
+      expect(result[0].timestamp).toEqual(data?.[0]?.[0])
+      expect(result[0].sender).toEqual(data?.[0]?.[1])
+      expect(result[0].gms).toEqual(data?.[0]?.[2])
+      expect(result[0].gms[0]).toEqual(data?.[0]?.[2]?.[0])
+      expect(result[0].gms[0].timestamp).toEqual(data?.[0]?.[2]?.[0]?.[0])
+      expect(result[0].gms[0].sender).toEqual(data?.[0]?.[2]?.[0]?.[1])
+      expect(result[1]).toEqual(data[1])
+      expect(Object.keys(result[1]).length !== data[1]?.length).toBeTruthy()
+      expect(result[1].timestamp).toEqual(data[1]?.[0])
+      expect(result[1].sender).toEqual(data[1]?.[1])
+      expect(result[1].gms).toEqual(data?.[1]?.[2])
+      expect(result[1].gms[0]).toEqual(data?.[1]?.[2]?.[0])
+      expect(result[1].gms[0].timestamp).toEqual(data?.[1]?.[2]?.[0]?.[0])
+      expect(result[1].gms[0].sender).toEqual(data?.[1]?.[2]?.[0]?.[1])
+    })
+
+    it('should return the data if in sync with ethers Result', () => {
+      const data: Result = [
+        Object.assign(
+          [
+            BigNumber.from(1654322661),
+            '0xa5cc3c03994DB5b0d9A5eEdD10CabaB0813678AC',
+            [
+              [
+                BigNumber.from(1654322661),
+                '0xa5cc3c03994DB5b0d9A5eEdD10CabaB0813678AC',
+              ],
+              [
+                BigNumber.from(1654322661),
+                '0xa5cc3c03994DB5b0d9A5eEdD10CabaB0813678AC',
+              ],
+            ],
+          ],
+          {
+            timestamp: BigNumber.from(1654322661),
+            sender: '0xa5cc3c03994DB5b0d9A5eEdD10CabaB0813678AC',
+            gms: [
+              Object.assign(
+                [
+                  BigNumber.from(1654322661),
+                  '0xa5cc3c03994DB5b0d9A5eEdD10CabaB0813678AC',
+                ],
+                {
+                  timestamp: BigNumber.from(1654322661),
+                  sender: '0xa5cc3c03994DB5b0d9A5eEdD10CabaB0813678AC',
+                },
+              ),
+              Object.assign(
+                [
+                  BigNumber.from(1654322661),
+                  '0xa5cc3c03994DB5b0d9A5eEdD10CabaB0813678AC',
+                ],
+                {
+                  timestamp: BigNumber.from(1654322661),
+                  sender: '0xa5cc3c03994DB5b0d9A5eEdD10CabaB0813678AC',
+                },
+              ),
+            ],
+          },
+        ),
+        Object.assign(
+          [
+            BigNumber.from(1654322662),
+            '0xa5cc3c03994DB5b0d9A5eEdD10CabaB0813678AD',
+            [
+              [
+                BigNumber.from(1654322661),
+                '0xa5cc3c03994DB5b0d9A5eEdD10CabaB0813678AC',
+              ],
+              [
+                BigNumber.from(1654322661),
+                '0xa5cc3c03994DB5b0d9A5eEdD10CabaB0813678AC',
+              ],
+            ],
+          ],
+          {
+            timestamp: BigNumber.from(1654322662),
+            sender: '0xa5cc3c03994DB5b0d9A5eEdD10CabaB0813678AD',
+            gms: [
+              Object.assign(
+                [
+                  BigNumber.from(1654322661),
+                  '0xa5cc3c03994DB5b0d9A5eEdD10CabaB0813678AC',
+                ],
+                {
+                  timestamp: BigNumber.from(1654322661),
+                  sender: '0xa5cc3c03994DB5b0d9A5eEdD10CabaB0813678AC',
+                },
+              ),
+              Object.assign(
+                [
+                  BigNumber.from(1654322661),
+                  '0xa5cc3c03994DB5b0d9A5eEdD10CabaB0813678AC',
+                ],
+                {
+                  timestamp: BigNumber.from(1654322661),
+                  sender: '0xa5cc3c03994DB5b0d9A5eEdD10CabaB0813678AC',
+                },
+              ),
+            ],
+          },
+        ),
+      ]
+      const result = parseContractResult({
+        contractInterface,
+        data,
+        functionName: 'complexTuples',
+      })
+
+      expect(Object.keys(result).length === data.length).toBeTruthy()
+      expect(result[0]).toEqual(data[0])
+      expect(Object.keys(result[0]).length !== data[0]?.length).toBeTruthy()
+      expect(result[0].timestamp).toEqual(data?.[0]?.timestamp)
+      expect(result[0].sender).toEqual(data?.[0]?.sender)
+      expect(result[0].gms[0].timestamp).toEqual(data[0].gms[0].timestamp)
+      expect(result[0].gms[0].sender).toEqual(data[0].gms[0].sender)
+      expect(result[1]).toEqual(data[1])
+      expect(Object.keys(result[1]).length !== data[1]?.length).toBeTruthy()
+      expect(result[1].timestamp).toEqual(data[1]?.timestamp)
+      expect(result[1].sender).toEqual(data[1]?.sender)
+      expect(result[1].gms[0].timestamp).toEqual(data[1].gms[0].timestamp)
+      expect(result[1].gms[0].sender).toEqual(data[1].gms[0].sender)
+    })
+  })
+
+  describe('withoutNames', () => {
+    it('should parse the data to an ethers Result if there are no named keys', () => {
+      const data = [
+        BigNumber.from(1654322661),
+        '0xa5cc3c03994DB5b0d9A5eEdD10CabaB0813678AC',
+      ]
+      const result = parseContractResult({
+        contractInterface,
+        data,
+        functionName: 'withoutNames',
+      })
+      expect(result).toEqual(data)
+      expect(Object.keys(result).length === data.length).toBeTruthy()
+    })
   })
 })
--- packages/react/src/utils/parseContractResult.ts
+++ packages/react/src/utils/parseContractResult.ts
@@ -1,6 +1,10 @@
 import { Contract, ContractInterface } from 'ethers/lib/ethers'
 import { FunctionFragment, Result } from 'ethers/lib/utils'
 
+function isPlainArray(value: unknown) {
+  return Array.isArray(value) && Object.keys(value).length === value.length
+}
+
 export function parseContractResult({
   contractInterface,
   data,
@@ -10,19 +14,68 @@ export function parseContractResult({
   data: Result
   functionName: string
 }) {
-  if (data && Array.isArray(data) && Object.keys(data).length === data.length) {
+  if (data && isPlainArray(data)) {
     const { fragments } = Contract.getInterface(contractInterface)
     const functionFragment = FunctionFragment.from(
       fragments.find((fragment) => fragment.name === functionName) || {},
     )
-    const dataObject = functionFragment.outputs?.reduce(
-      (dataObject, output, i) => ({
-        ...dataObject,
-        [output.name]: data[i],
-      }),
+    return getFunctionData({
+      data,
+      outputs: functionFragment.outputs,
+    })
+  }
+  return data
+}
+
+function getFunctionData({
+  data,
+  outputs,
+  isTuple = outputs?.[0]?.type === 'tuple[]',
+}: {
+  data: Result
+  outputs: FunctionFragment['outputs']
+  isTuple?: boolean
+}): Result {
+  if (outputs?.[0]?.type === 'tuple[]') {
+    // If all the tuple items are already in sync with the ethers Result,
+    // return the data.
+    if (data.some((data) => !isPlainArray(data))) {
+      return data
+    }
+
+    // Otherwise, traverse and parse the data to an ethers Result
+    return getFunctionData({
+      data,
+      outputs: outputs?.[0].components,
+      isTuple: true,
+    })
+  }
+
+  const data_ =
+    isPlainArray(data) && !data.some((data) => !Array.isArray(data))
+      ? data
+      : [data]
+  const result = data_.map((data) => {
+    const dataObject = outputs?.reduce(
+      (dataObject, output, i) =>
+        output.name
+          ? {
+              ...dataObject,
+              [output.name]:
+                output.type === 'tuple[]'
+                  ? getFunctionData({
+                      data: data[i],
+                      outputs: output.components,
+                      isTuple: true,
+                    })
+                  : data[i],
+            }
+          : dataObject,
       {},
     )
     return Object.assign(data, dataObject)
-  }
-  return data
+  })
+
+  if (!isTuple) return result[0]
+  return result
 }
