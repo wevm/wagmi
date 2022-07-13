1c322e8e2f1de16f53cce609ab4b5a29921262a8
moxey.eth <jakemoxey@gmail.com>
2022-07-14 08:11:49 +1000

    wip: add send transaction example

--- docs/components/examples/SendTransaction.tsx
+++ docs/components/examples/SendTransaction.tsx
@@ -1,10 +1,11 @@
-import { Button, Input, Stack, Text } from 'degen'
+import { Box, Button, Input, Stack, Text } from 'degen'
 import { parseEther } from 'ethers/lib/utils'
 import * as React from 'react'
 import {
   useAccount,
   usePrepareSendTransaction,
   useSendTransaction,
+  useWaitForTransaction,
 } from 'wagmi'
 
 import { PreviewWrapper } from '../core'
@@ -44,41 +45,61 @@ export function SendTransaction() {
       value: debouncedValue ? parseEther(debouncedValue) : undefined,
     },
   })
-  const { data, error, isLoading, isError, isSuccess, sendTransaction } =
+  const { data, error, isLoading, isError, sendTransaction } =
     useSendTransaction(config)
+  const { isLoading: isConfirming, isSuccess } = useWaitForTransaction({
+    hash: data?.hash,
+  })
 
   if (isConnected)
     return (
       <PreviewWrapper>
-        <Stack>
-          <Account />
-          <Input
-            onChange={(e) => setTo(e.target.value)}
-            label="Recipient"
-            placeholder="0xA0Cf…251e"
-            value={to}
-          />
-          <Input
-            onChange={(e) => setValue(e.target.value)}
-            label="Amount (ether)"
-            placeholder="0.05"
-            type="number"
-            value={value}
-          />
-          <Button
-            disabled={!sendTransaction || !to || !value}
-            loading={isPreparing || isLoading}
-            onClick={() => sendTransaction?.()}
-            width="full"
-          >
-            Send
-          </Button>
-          {isSuccess && <Text>Success! Hash: {data?.hash}</Text>}
-          {isPrepareError && (
-            <Text color="red">Error: {prepareError?.message}</Text>
-          )}
-          {isError && <Text color="red">Error: {error?.message}</Text>}
-        </Stack>
+        <Box
+          as="form"
+          onSubmit={(e) => {
+            e.preventDefault()
+            if (isLoading || isConfirming) return
+            sendTransaction?.()
+          }}
+        >
+          <Stack>
+            <Account />
+            <Input
+              onChange={(e) => setTo(e.target.value)}
+              label="Recipient"
+              placeholder="0xA0Cf…251e"
+              value={to}
+            />
+            <Input
+              onChange={(e) => setValue(e.target.value)}
+              label="Amount (ether)"
+              placeholder="0.05"
+              type="number"
+              value={value}
+            />
+            <Button
+              disabled={!sendTransaction || !to || !value}
+              loading={isPreparing || isLoading || isConfirming}
+              width="full"
+            >
+              {isLoading
+                ? 'Check wallet...'
+                : isConfirming
+                ? 'Sending...'
+                : 'Send'}
+            </Button>
+            {isSuccess && (
+              <Text>
+                Success!{' '}
+                <a href={`https://etherscan.io/tx/${data?.hash}`}>Etherscan</a>
+              </Text>
+            )}
+            {isPrepareError && (
+              <Text color="red">Error: {prepareError?.message}</Text>
+            )}
+            {isError && <Text color="red">Error: {error?.message}</Text>}
+          </Stack>
+        </Box>
       </PreviewWrapper>
     )
 
