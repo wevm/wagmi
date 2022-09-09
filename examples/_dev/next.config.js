a5bfcff72aa46dc5d9ed2700196135edbb38efc8
moxey.eth <jakemoxey@gmail.com>
2022-09-09 11:11:49 +1000

    Update next.config.js

--- examples/_dev/next.config.js
+++ examples/_dev/next.config.js
@@ -2,8 +2,5 @@ const withPreconstruct = require('@preconstruct/next')
 
 /** @type {import('next').NextConfig} */
 module.exports = withPreconstruct({
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   reactStrictMode: true,
 })
