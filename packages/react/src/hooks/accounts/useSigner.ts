0664029b8b1aad707cfb24e8e2bcff092e1e4636
moxey.eth <jakemoxey@gmail.com>
2022-09-27 15:42:23 +1000

    fix: ensure useSigner result does not broadcast to other useSigner instances

--- packages/react/src/hooks/accounts/useSigner.ts
+++ packages/react/src/hooks/accounts/useSigner.ts
@@ -39,7 +39,7 @@ export function useSigner<TSigner extends Signer>({
     ReturnType<typeof queryKey>
   >(queryKey({ chainId }), queryFn, {
     cacheTime: 0,
-    staleTime: 0,
+    staleTime: Infinity,
     suspense,
     onError,
     onSettled,
