0e6e70cb6297a9a8771e531b750d014b1b183589
moxey.eth <jakemoxey@gmail.com>
2022-09-28 07:31:11 +1000

    fix: make watchBlockNumber chain aware + fix listener

--- packages/core/src/actions/network-status/watchBlockNumber.ts
+++ packages/core/src/actions/network-status/watchBlockNumber.ts
@@ -3,9 +3,10 @@ import shallow from 'zustand/shallow'
 import { getClient } from '../../client'
 import { Provider } from '../../types'
 import { debounce } from '../../utils'
+import { getProvider, getWebSocketProvider } from '../providers'
 import { FetchBlockNumberResult, fetchBlockNumber } from './fetchBlockNumber'
 
-export type WatchBlockNumberArgs = { listen: boolean }
+export type WatchBlockNumberArgs = { chainId?: number; listen: boolean }
 export type WatchBlockNumberCallback = (
   blockNumber: FetchBlockNumberResult,
 ) => void
@@ -15,13 +16,14 @@ export function watchBlockNumber(
   callback: WatchBlockNumberCallback,
 ) {
   let previousProvider: Provider
+  const debouncedCallback = debounce(callback, 1)
+
   const createListener = (provider: Provider) => {
     // We need to debounce the listener as we want to opt-out
     // of the behavior where ethers emits a "block" event for
     // every block that was missed in between the `pollingInterval`.
     // We are setting a wait time of 1 as emitting an event in
     // ethers takes ~0.1ms.
-    const debouncedCallback = debounce(callback, 1)
     if (previousProvider) {
       previousProvider?.off('block', debouncedCallback)
     }
@@ -29,18 +31,20 @@ export function watchBlockNumber(
     previousProvider = provider
   }
 
-  const client = getClient()
-  const provider_ = client.webSocketProvider ?? client.provider
+  const provider_ =
+    getWebSocketProvider({ chainId: args.chainId }) ??
+    getProvider({ chainId: args.chainId })
   if (args.listen) createListener(provider_)
 
+  const client = getClient()
   const unsubscribe = client.subscribe(
     ({ provider, webSocketProvider }) => ({ provider, webSocketProvider }),
     async ({ provider, webSocketProvider }) => {
       const provider_ = webSocketProvider ?? provider
-      if (args.listen && provider_) {
+      if (args.listen && !args.chainId && provider_) {
         createListener(provider_)
       }
-      callback(await fetchBlockNumber())
+      callback(await fetchBlockNumber({ chainId: args.chainId }))
     },
     {
       equalityFn: shallow,
@@ -48,6 +52,6 @@ export function watchBlockNumber(
   )
   return () => {
     unsubscribe()
-    provider_?.off('block', callback)
+    provider_?.off('block', debouncedCallback)
   }
 }
