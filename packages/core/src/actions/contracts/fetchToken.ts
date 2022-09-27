381531ee25ea45a6fb4d0e2fe97e309af41b34dd
jxom <jakemoxey@gmail.com>
2022-09-27 15:06:10 +1000

    enhancement: support ERC20 contracts that use `bytes32` (#993)
    * enhancement: support bytes32 erc20
    
    * chore: changesets
    
    * chore: add note
    
    * refactor: convert abi to json
    
    * feat: support bytes32 fetchToken

--- packages/core/src/actions/contracts/fetchToken.ts
+++ packages/core/src/actions/contracts/fetchToken.ts
@@ -1,9 +1,10 @@
 import { BigNumber } from 'ethers/lib/ethers'
-import { formatUnits } from 'ethers/lib/utils'
+import { formatUnits, parseBytes32String } from 'ethers/lib/utils'
 
-import { erc20ABI } from '../../constants'
+import { erc20ABI, erc20ABI_bytes32 } from '../../constants'
+import { ContractResultDecodeError } from '../../errors'
 import { Unit } from '../../types'
-import { readContracts } from '../contracts'
+import { GetContractArgs, readContracts } from '../contracts'
 
 export type FetchTokenArgs = {
   /** Address of ERC-20 token */
@@ -29,31 +30,56 @@ export async function fetchToken({
   chainId,
   formatUnits: units = 'ether',
 }: FetchTokenArgs): Promise<FetchTokenResult> {
-  const erc20Config = {
-    addressOrName: address,
-    contractInterface: erc20ABI,
-    chainId,
-  }
-  const [decimals, name, symbol, totalSupply] = await readContracts<
-    [number, string, string, BigNumber]
-  >({
-    allowFailure: false,
-    contracts: [
-      { ...erc20Config, functionName: 'decimals' },
-      { ...erc20Config, functionName: 'name' },
-      { ...erc20Config, functionName: 'symbol' },
-      { ...erc20Config, functionName: 'totalSupply' },
-    ],
-  })
+  async function fetchToken_({
+    contractInterface,
+  }: {
+    contractInterface: GetContractArgs['contractInterface']
+  }) {
+    const erc20Config = {
+      addressOrName: address,
+      contractInterface,
+      chainId,
+    }
+    const [decimals, name, symbol, totalSupply] = await readContracts<
+      [number, string, string, BigNumber]
+    >({
+      allowFailure: false,
+      contracts: [
+        { ...erc20Config, functionName: 'decimals' },
+        { ...erc20Config, functionName: 'name' },
+        { ...erc20Config, functionName: 'symbol' },
+        { ...erc20Config, functionName: 'totalSupply' },
+      ],
+    })
 
-  return {
-    address,
-    decimals,
-    name,
-    symbol,
-    totalSupply: {
-      formatted: formatUnits(totalSupply, units),
-      value: totalSupply,
-    },
+    return {
+      address,
+      decimals,
+      name,
+      symbol,
+      totalSupply: {
+        formatted: formatUnits(totalSupply, units),
+        value: totalSupply,
+      },
+    }
+  }
+
+  try {
+    return await fetchToken_({ contractInterface: erc20ABI })
+  } catch (err) {
+    // In the chance that there is an error upon decoding the contract result,
+    // it could be likely that the contract data is represented as bytes32 instead
+    // of a string.
+    if (err instanceof ContractResultDecodeError) {
+      const { name, symbol, ...rest } = await fetchToken_({
+        contractInterface: erc20ABI_bytes32,
+      })
+      return {
+        name: parseBytes32String(name),
+        symbol: parseBytes32String(symbol),
+        ...rest,
+      }
+    }
+    throw err
   }
 }
--- packages/core/src/constants/abis.ts
+++ packages/core/src/constants/abis.ts
@@ -13,6 +13,208 @@ export const erc20ABI = [
   'function transferFrom(address _from, address _to, uint256 _value) public returns (bool success)',
 ]
 
+/**
+ * [bytes32-flavored ERC-20](https://docs.makerdao.com/smart-contract-modules/mkr-module#4.-gotchas-potential-source-of-user-error)
+ * for tokens (ie. Maker) that use bytes32 instead of string.
+ */
+export const erc20ABI_bytes32 = [
+  {
+    type: 'event',
+    name: 'Approval',
+    inputs: [
+      {
+        indexed: true,
+        name: 'owner',
+        type: 'address',
+      },
+      {
+        indexed: true,
+        name: 'spender',
+        type: 'address',
+      },
+      {
+        indexed: false,
+        name: 'value',
+        type: 'uint256',
+      },
+    ],
+  },
+  {
+    type: 'event',
+    name: 'Transfer',
+    inputs: [
+      {
+        indexed: true,
+        name: 'from',
+        type: 'address',
+      },
+      {
+        indexed: true,
+        name: 'to',
+        type: 'address',
+      },
+      {
+        indexed: false,
+        name: 'value',
+        type: 'uint256',
+      },
+    ],
+  },
+  {
+    type: 'function',
+    name: 'allowance',
+    stateMutability: 'view',
+    inputs: [
+      {
+        name: 'owner',
+        type: 'address',
+      },
+      {
+        name: 'spender',
+        type: 'address',
+      },
+    ],
+    outputs: [
+      {
+        name: '',
+        type: 'uint256',
+      },
+    ],
+  },
+  {
+    type: 'function',
+    name: 'approve',
+    stateMutability: 'nonpayable',
+    inputs: [
+      {
+        name: 'spender',
+        type: 'address',
+      },
+      {
+        name: 'amount',
+        type: 'uint256',
+      },
+    ],
+    outputs: [
+      {
+        name: '',
+        type: 'bool',
+      },
+    ],
+  },
+  {
+    type: 'function',
+    name: 'balanceOf',
+    stateMutability: 'view',
+    inputs: [
+      {
+        name: 'account',
+        type: 'address',
+      },
+    ],
+    outputs: [
+      {
+        name: '',
+        type: 'uint256',
+      },
+    ],
+  },
+  {
+    type: 'function',
+    name: 'decimals',
+    stateMutability: 'view',
+    inputs: [],
+    outputs: [
+      {
+        name: '',
+        type: 'uint8',
+      },
+    ],
+  },
+  {
+    type: 'function',
+    name: 'name',
+    stateMutability: 'view',
+    inputs: [],
+    outputs: [
+      {
+        name: '',
+        type: 'bytes32',
+      },
+    ],
+  },
+  {
+    type: 'function',
+    name: 'symbol',
+    stateMutability: 'view',
+    inputs: [],
+    outputs: [
+      {
+        name: '',
+        type: 'bytes32',
+      },
+    ],
+  },
+  {
+    type: 'function',
+    name: 'totalSupply',
+    stateMutability: 'view',
+    inputs: [],
+    outputs: [
+      {
+        name: '',
+        type: 'uint256',
+      },
+    ],
+  },
+  {
+    type: 'function',
+    name: 'transfer',
+    stateMutability: 'nonpayable',
+    inputs: [
+      {
+        name: 'recipient',
+        type: 'address',
+      },
+      {
+        name: 'amount',
+        type: 'uint256',
+      },
+    ],
+    outputs: [
+      {
+        name: '',
+        type: 'bool',
+      },
+    ],
+  },
+  {
+    type: 'function',
+    name: 'transferFrom',
+    stateMutability: 'nonpayable',
+    inputs: [
+      {
+        name: 'sender',
+        type: 'address',
+      },
+      {
+        name: 'recipient',
+        type: 'address',
+      },
+      {
+        name: 'amount',
+        type: 'uint256',
+      },
+    ],
+    outputs: [
+      {
+        name: '',
+        type: 'bool',
+      },
+    ],
+  },
+] as const
+
 // https://ethereum.org/en/developers/docs/standards/tokens/erc-721
 export const erc721ABI = [
   'event Approval(address indexed _owner, address indexed _approved, uint256 indexed _tokenId)',
