33055a37aa8dd34e7ec9577e2b3287b63cf4c37f
Matthew Hausman <mattadamhausman@gmail.com>
2022-10-24 16:19:38 -0400

    Update useAccount.ts

--- packages/vue/src/hooks/accounts/useAccount.ts
+++ packages/vue/src/hooks/accounts/useAccount.ts
@@ -4,20 +4,18 @@ import {
   getAccount,
   watchAccount,
 } from '@wagmi/core'
-import { onScopeDispose, ref, unref, watch } from 'vue-demi'
+import { onScopeDispose, ref, watch } from 'vue-demi'
 
-import { MaybeRef } from '../../types'
-
-type UseAccountConfig = {
+export type UseAccountConfig = {
   /** Function to invoke when connected */
   onConnect?({
     address,
     connector,
     isReconnected,
   }: {
-    address?: MaybeRef<GetAccountResult['address']>
-    connector?: MaybeRef<Connector>
-    isReconnected: MaybeRef<boolean>
+    address?: GetAccountResult['address']
+    connector?: GetAccountResult['connector']
+    isReconnected: boolean
   }): void
   /** Function to invoke when disconnected */
   onDisconnect?(): void
@@ -36,25 +34,23 @@ export const useAccount = ({
   } = { current: getAccount() }
 
   watch(account, () => {
-    const plainOnConnect = unref(onConnect)
-    const plainOnDisconnect = unref(onDisconnect)
     if (
-      !!plainOnConnect &&
+      !!onConnect &&
       previousAccount.current?.status !== 'connected' &&
       account.value.status === 'connected'
     )
-      plainOnConnect({
+      onConnect({
         address: account.value.address,
         connector: account.value.connector as Connector,
         isReconnected: previousAccount.current?.status === 'reconnecting',
       })
 
     if (
-      !!plainOnDisconnect &&
+      !!onDisconnect &&
       previousAccount.current?.status == 'connected' &&
       account.value.status === 'disconnected'
     )
-      plainOnDisconnect()
+      onDisconnect()
     previousAccount.current = account.value as GetAccountResult
   })
   onScopeDispose(() => {
