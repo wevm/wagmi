ce2e0f4a46b8fd1c509ead552012ef4c072a525b
awkweb <tom@meagher.co>
2022-11-07 13:57:47 -0500

    feat: add support for trust wallet browser extension (#1250)

--- packages/core/src/utils/getInjectedName.test.ts
+++ packages/core/src/utils/getInjectedName.test.ts
@@ -4,6 +4,11 @@ import { getInjectedName } from './getInjectedName'
 
 describe.each([
   { ethereum: undefined, expected: 'Injected' },
+  { ethereum: { isAvalanche: true }, expected: 'Core Wallet' },
+  {
+    ethereum: { isAvalanche: true, isMetaMask: true },
+    expected: 'Core Wallet',
+  },
   { ethereum: { isBitKeep: true }, expected: 'BitKeep' },
   { ethereum: { isBraveWallet: true }, expected: 'Brave Wallet' },
   {
@@ -13,6 +18,18 @@ describe.each([
   { ethereum: { isCoinbaseWallet: true }, expected: 'Coinbase Wallet' },
   { ethereum: { isExodus: true }, expected: 'Exodus' },
   { ethereum: { isFrame: true }, expected: 'Frame' },
+  { ethereum: { isKuCoinWallet: true }, expected: 'KuCoin Wallet' },
+  {
+    ethereum: { isKuCoinWallet: true, isMetaMask: true },
+    expected: 'KuCoin Wallet',
+  },
+  {
+    ethereum: { isMathWallet: true, isMetaMask: true },
+    expected: 'MathWallet',
+  },
+  { ethereum: { isOneInchIOSWallet: true }, expected: '1inch Wallet' },
+  { ethereum: { isOneInchAndroidWallet: true }, expected: '1inch Wallet' },
+  { ethereum: { isPortal: true }, expected: 'Ripio Portal' },
   { ethereum: { isTally: true }, expected: 'Tally' },
   {
     ethereum: { isTokenPocket: true, isMetaMask: true },
@@ -20,13 +37,7 @@ describe.each([
   },
   { ethereum: { isTokenary: true, isMetaMask: true }, expected: 'Tokenary' },
   { ethereum: { isTrust: true }, expected: 'Trust Wallet' },
-  { ethereum: { isOneInchIOSWallet: true }, expected: '1inch Wallet' },
-  { ethereum: { isOneInchAndroidWallet: true }, expected: '1inch Wallet' },
-  { ethereum: { isPortal: true }, expected: 'Ripio Portal' },
-  {
-    ethereum: { isMathWallet: true, isMetaMask: true },
-    expected: 'MathWallet',
-  },
+  { ethereum: { isTrustWallet: true }, expected: 'Trust Wallet' },
   { ethereum: { isMetaMask: true }, expected: 'MetaMask' },
   {
     ethereum: { providers: [{ isMetaMask: true }, { isCoinbaseWallet: true }] },
@@ -47,16 +58,6 @@ describe.each([
     expected: ['MetaMask', 'Unknown Wallet #1', 'Unknown Wallet #2'],
   },
   { ethereum: {}, expected: 'Injected' },
-  { ethereum: { isAvalanche: true }, expected: 'Core Wallet' },
-  {
-    ethereum: { isAvalanche: true, isMetaMask: true },
-    expected: 'Core Wallet',
-  },
-  { ethereum: { isKuCoinWallet: true }, expected: 'KuCoin Wallet' },
-  {
-    ethereum: { isKuCoinWallet: true, isMetaMask: true },
-    expected: 'KuCoin Wallet',
-  },
 ])('getInjectedName($ethereum)', ({ ethereum, expected }) => {
   it(`returns ${expected}`, () => {
     expect(getInjectedName(<any>ethereum)).toEqual(expected)
