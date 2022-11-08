9f75fd531c23f8af33247e4b4a32df963f57c4c2
Gancho Radkov <ganchoradkov93@gmail.com>
2022-11-08 15:41:24 +0200

    feat: adds tests for v1 & v2

--- packages/core/src/connectors/walletConnect.test.ts
+++ packages/core/src/connectors/walletConnect.test.ts
@@ -1,3 +1,4 @@
+import WalletConnectProvider from '@walletconnect/ethereum-provider'
 import { describe, expect, it } from 'vitest'
 
 import { chain, defaultChains } from '../constants'
@@ -13,6 +14,48 @@ describe('WalletConnectConnector', () => {
         },
       },
     })
+    expect(connector.name).toEqual('WalletConnect')
+  })
+  it('should use v1 by default', async () => {
+    const connector = new WalletConnectConnector({
+      chains: defaultChains,
+      options: {
+        rpc: {
+          [chain.foundry.id]: chain.foundry.rpcUrls.default,
+        },
+      },
+    })
+    expect((await connector.getProvider()) instanceof WalletConnectProvider).to
+      .be.true
+    expect(connector.name).toEqual('WalletConnect')
+  })
+  it('should use v1 as configured by options', async () => {
+    const connector = new WalletConnectConnector({
+      chains: defaultChains,
+      options: {
+        rpc: {
+          [chain.foundry.id]: chain.foundry.rpcUrls.default,
+        },
+        version: '1',
+      },
+    })
+    expect((await connector.getProvider()) instanceof WalletConnectProvider).to
+      .be.true
+    expect(connector.name).toEqual('WalletConnect')
+  })
+  it('should use v2 as configured by options', async () => {
+    const connector = new WalletConnectConnector({
+      chains: defaultChains,
+      options: {
+        rpc: {
+          [chain.foundry.id]: chain.foundry.rpcUrls.default,
+        },
+        version: '2',
+      },
+    })
+    expect(connector.getProvider()).rejects.toThrowError(
+      'Please get a WalletConnect v2 projectID from https://cloud.walletconnect.com/',
+    )
     expect(connector.name).toEqual('WalletConnect')
   })
 })
