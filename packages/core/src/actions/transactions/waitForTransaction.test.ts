0f8fa50ee13bd0a390af895e30d835919ab9199c
Tom Meagher <tom@meagher.co>
2022-11-11 17:00:46 -0500

    feat: throw when waiting on reverted transaction

--- packages/core/src/actions/transactions/waitForTransaction.test.ts
+++ packages/core/src/actions/transactions/waitForTransaction.test.ts
@@ -61,4 +61,17 @@ describe('waitForTransaction', () => {
       expect(receipt.transactionHash).toEqual(result.hash)
     })
   })
+
+  describe('behavior', () => {
+    it('reverts', async () => {
+      await expect(
+        waitForTransaction({
+          // https://etherscan.io/tx/0x227025716535219d4c3737172c9c1507475e7a3246c8efcdc050ac59ed40b68e
+          hash: '0x227025716535219d4c3737172c9c1507475e7a3246c8efcdc050ac59ed40b68e',
+        }),
+      ).rejects.toThrowErrorMatchingInlineSnapshot(
+        '"missing revert data in call exception; Transaction reverted without a reason string [ See: https://links.ethers.org/v5-errors-CALL_EXCEPTION ] (data=\\"0x\\", transaction={\\"from\\":\\"0xA0Cf798816D4b9b9866b5330EEa46a18382f251e\\",\\"to\\":\\"0x283Af0B28c62C092C9727F1Ee09c02CA627EB7F5\\",\\"type\\":0,\\"accessList\\":null}, error={\\"reason\\":\\"processing response error\\",\\"code\\":\\"SERVER_ERROR\\",\\"body\\":\\"{\\\\\\"jsonrpc\\\\\\":\\\\\\"2.0\\\\\\",\\\\\\"id\\\\\\":46,\\\\\\"error\\\\\\":{\\\\\\"code\\\\\\":-32603,\\\\\\"message\\\\\\":\\\\\\"Fork Error: JsonRpcClientError(JsonRpcError(JsonRpcError { code: -32000, message: \\\\\\\\\\\\\\"execution reverted\\\\\\\\\\\\\\", data: None }))\\\\\\"}}\\",\\"error\\":{\\"code\\":-32603},\\"requestBody\\":\\"{\\\\\\"method\\\\\\":\\\\\\"eth_call\\\\\\",\\\\\\"params\\\\\\":[{\\\\\\"type\\\\\\":\\\\\\"0x0\\\\\\",\\\\\\"from\\\\\\":\\\\\\"0xa0cf798816d4b9b9866b5330eea46a18382f251e\\\\\\",\\\\\\"to\\\\\\":\\\\\\"0x283af0b28c62c092c9727f1ee09c02ca627eb7f5\\\\\\"},\\\\\\"0xbeea0b\\\\\\"],\\\\\\"id\\\\\\":46,\\\\\\"jsonrpc\\\\\\":\\\\\\"2.0\\\\\\"}\\",\\"requestMethod\\":\\"POST\\",\\"url\\":\\"http://127.0.0.1:8545\\"}, code=CALL_EXCEPTION, version=providers/5.7.1)"',
+      )
+    })
+  })
 })
