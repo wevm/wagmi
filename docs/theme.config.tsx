08379d928160b5c80603a74552e0aa3d824dbfe7
Tom Meagher <tom@meagher.co>
2022-12-04 17:24:22 -0500

    fix: lint errors

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -1,4 +1,3 @@
-import { useTheme } from 'next-themes'
 import { useRouter } from 'next/router'
 import type { DocsThemeConfig } from 'nextra-theme-docs'
 import { useConfig } from 'nextra-theme-docs'
