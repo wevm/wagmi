cc128930bdac8a6933908035289f38add33169c6
Tom Meagher <tom@meagher.co>
2022-12-19 13:04:06 -0500

    fix: build

--- packages/cli/tsup.config.ts
+++ packages/cli/tsup.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'tsup'
 
 import { getConfig } from '../../scripts/tsup'
-import { dependencies, exports, peerDependencies } from './package.json'
+import { dependencies, exports } from './package.json'
 
 export default defineConfig(
   getConfig({
     entry: ['src/index.ts', 'src/cli.ts', 'src/config.ts'],
     exports,
-    external: [...Object.keys(dependencies), ...Object.keys(peerDependencies)],
+    external: [...Object.keys(dependencies)],
   }),
 )
