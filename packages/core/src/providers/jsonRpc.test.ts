acfc5fc8fc1deb082ecaf50687ab31142114fecd
0xflick <cmdrnft@protonmail.com>
2022-12-20 21:10:48 -0600

    test: verify that ens registry can be changed for the easier to test providers

---
+++ packages/core/src/providers/jsonRpc.test.ts
@@ -0,0 +1,49 @@
+import { beforeAll, describe, expect, it } from 'vitest'
+
+import type { Chain } from '../'
+import { testChains } from '../../test/utils'
+import { jsonRpcProvider } from './jsonRpc'
+
+describe('jsonRpc', () => {
+  let foundryChain: Chain
+  let foundryChainCustomRegistry: Chain
+
+  beforeAll(() => {
+    foundryChain = testChains[0]!
+    foundryChainCustomRegistry = {
+      ...foundryChain,
+      contracts: {
+        ...foundryChain.contracts,
+        ensRegistry: {
+          address: '0xcc984772c14382b0d429c82d37e6925bffa3ee3c',
+        },
+      },
+    }
+  })
+
+  describe('ens lookup', () => {
+    it('default', async () => {
+      const { provider: providerFactory } = jsonRpcProvider({
+        rpc: () => foundryChain.rpcUrls['default'].http[0],
+      })(foundryChain)!
+      const provider = providerFactory()
+      expect(
+        await provider.lookupAddress(
+          '0xA0Cf798816D4b9b9866b5330EEa46a18382f251e',
+        ),
+      ).toMatchInlineSnapshot(`"awkweb.eth"`)
+    })
+
+    it('default', async () => {
+      const { provider: providerFactory } = jsonRpcProvider({
+        rpc: () => foundryChain.rpcUrls['default'].http[0],
+      })(foundryChainCustomRegistry)!
+      const provider = providerFactory()
+      expect(
+        await provider.lookupAddress(
+          '0xA0Cf798816D4b9b9866b5330EEa46a18382f251e',
+        ),
+      ).toMatchInlineSnapshot(`null`)
+    })
+  })
+})
---
+++ packages/core/src/providers/public.test.ts
@@ -0,0 +1,47 @@
+import { beforeAll, describe, expect, it } from 'vitest'
+
+import type { Chain } from '../'
+import { testChains } from '../../test/utils'
+import { publicProvider } from './public'
+
+describe('publicProviders', () => {
+  let foundryChain: Chain
+  let foundryChainCustomRegistry: Chain
+
+  beforeAll(() => {
+    foundryChain = testChains[0]!
+    foundryChainCustomRegistry = {
+      ...foundryChain,
+      contracts: {
+        ...foundryChain.contracts,
+        ensRegistry: {
+          address: '0xcc984772c14382b0d429c82d37e6925bffa3ee3c',
+        },
+      },
+    }
+  })
+
+  describe('ens lookup', () => {
+    it('default', async () => {
+      const { provider: providerFactory } = publicProvider()(foundryChain)!
+      const provider = providerFactory()
+      expect(
+        await provider.lookupAddress(
+          '0xA0Cf798816D4b9b9866b5330EEa46a18382f251e',
+        ),
+      ).toMatchInlineSnapshot(`"awkweb.eth"`)
+    })
+
+    it('default', async () => {
+      const { provider: providerFactory } = publicProvider()(
+        foundryChainCustomRegistry,
+      )!
+      const provider = providerFactory()
+      expect(
+        await provider.lookupAddress(
+          '0xA0Cf798816D4b9b9866b5330EEa46a18382f251e',
+        ),
+      ).toMatchInlineSnapshot(`null`)
+    })
+  })
+})
