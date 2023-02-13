cb707f01cbdcc62a70cf5c8a162d77948d6b6a56
awkweb <tom@meagher.co>
2023-02-13 11:26:15 -0500

    feat: sourcify cli plugin (#1841)
    * chore: update vhs
    
    * sourcify cli plugin
    
    * parse sourcify response
    
    * fix typecheck
    
    * changeset
    
    * add docs
    
    * restore correct public assets
    
    * refactor: sourcify plugin errors
    
    * test: update snapshot
    
    ---------
    
    Co-authored-by: wslyvh <wslyvh@users.noreply.github.com>

--- packages/cli/test/constants.ts
+++ packages/cli/test/constants.ts
@@ -198,3 +198,121 @@ export const wagmiAbi = [
     type: 'function',
   },
 ] as const
+
+export const depositAbi = [
+  {
+    inputs: [],
+    stateMutability: 'nonpayable',
+    type: 'constructor',
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: 'bytes',
+        name: 'pubkey',
+        type: 'bytes',
+      },
+      {
+        indexed: false,
+        internalType: 'bytes',
+        name: 'withdrawal_credentials',
+        type: 'bytes',
+      },
+      {
+        indexed: false,
+        internalType: 'bytes',
+        name: 'amount',
+        type: 'bytes',
+      },
+      {
+        indexed: false,
+        internalType: 'bytes',
+        name: 'signature',
+        type: 'bytes',
+      },
+      {
+        indexed: false,
+        internalType: 'bytes',
+        name: 'index',
+        type: 'bytes',
+      },
+    ],
+    name: 'DepositEvent',
+    type: 'event',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'bytes',
+        name: 'pubkey',
+        type: 'bytes',
+      },
+      {
+        internalType: 'bytes',
+        name: 'withdrawal_credentials',
+        type: 'bytes',
+      },
+      {
+        internalType: 'bytes',
+        name: 'signature',
+        type: 'bytes',
+      },
+      {
+        internalType: 'bytes32',
+        name: 'deposit_data_root',
+        type: 'bytes32',
+      },
+    ],
+    name: 'deposit',
+    outputs: [],
+    stateMutability: 'payable',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'get_deposit_count',
+    outputs: [
+      {
+        internalType: 'bytes',
+        name: '',
+        type: 'bytes',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'get_deposit_root',
+    outputs: [
+      {
+        internalType: 'bytes32',
+        name: '',
+        type: 'bytes32',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'bytes4',
+        name: 'interfaceId',
+        type: 'bytes4',
+      },
+    ],
+    name: 'supportsInterface',
+    outputs: [
+      {
+        internalType: 'bool',
+        name: '',
+        type: 'bool',
+      },
+    ],
+    stateMutability: 'pure',
+    type: 'function',
+  },
+]
