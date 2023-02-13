7ecc5fae54f577e5714a81a85b6e282c4fb9b2af
Tom Meagher <tom@meagher.co>
2023-02-13 11:15:47 -0500

    test: update snaphsots

--- packages/react/src/hooks/accounts/useSwitchNetwork.test.ts
+++ packages/react/src/hooks/accounts/useSwitchNetwork.test.ts
@@ -329,6 +329,10 @@ describe('useSwitchNetwork', () => {
                 "ensRegistry": {
                   "address": "0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e",
                 },
+                "ensUniversalResolver": {
+                  "address": "0x687c30Cc44bFA39A1449e86E172BF002E7b3f0b0",
+                  "blockCreated": 7725078,
+                },
                 "multicall3": {
                   "address": "0xca11bde05977b3631167028862be2a173976ca11",
                   "blockCreated": 6507670,
