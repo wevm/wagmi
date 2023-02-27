8d9afa3fd762988fd41d879a07418f5dd3ef2d5f
moxey.eth <jakemoxey@gmail.com>
2023-02-28 10:08:29 +1100

    chore: update references

--- examples/_dev/src/pages/_app.tsx
+++ examples/_dev/src/pages/_app.tsx
@@ -43,7 +43,6 @@ const client = createClient({
       chains,
       options: {
         projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID ?? '',
-        qrcode: true,
       },
     }),
     new LedgerConnector({
