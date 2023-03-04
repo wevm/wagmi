312eb64718c934a8eb81ee0e434e95a121ab074d
Gabriel <gabrielgcr45@gmail.com>
2023-03-03 22:53:13 -0300

    changed useProvider and docs

--- packages/core/src/actions/providers/getProvider.ts
+++ packages/core/src/actions/providers/getProvider.ts
@@ -8,10 +8,11 @@ export type GetProviderArgs = {
 
 export type GetProviderResult<TProvider extends Provider = Provider> = TProvider
 
-export function getProvider<TProvider extends Provider = Provider>({
-  chainId,
-}: GetProviderArgs = {}): GetProviderResult<TProvider> {
+export function getProvider<TProvider extends Provider = Provider>(
+  props: GetProviderArgs = {},
+): GetProviderResult<TProvider> {
   const client = getClient<TProvider>()
-  if (chainId) return client.getProvider({ chainId }) || client.provider
+  if (props.chainId)
+    return client.getProvider({ chainId: props.chainId }) || client.provider
   return client.provider
 }
--- packages/solid/src/hooks/providers/useProvider.ts
+++ packages/solid/src/hooks/providers/useProvider.ts
@@ -1,9 +1,4 @@
-import type {
-  GetProviderResult,
-  Provider,
-  WatchProviderCallback,
-} from '@wagmi/core'
-import { getClient } from '@wagmi/core'
+import { getProvider, watchProvider } from '@wagmi/core'
 import type { Accessor } from 'solid-js'
 import { createEffect, createSignal, onCleanup } from 'solid-js'
 
@@ -13,36 +8,13 @@ export type GetProviderArgs = {
 
 export type UseProviderArgs = Partial<GetProviderArgs>
 
-function getProvider<TProvider extends Provider = Provider>(
-  props: GetProviderArgs = {},
-): GetProviderResult<TProvider> {
-  const client = getClient<TProvider>()
-  if (props?.chainId)
-    return client.getProvider({ chainId: props?.chainId() }) || client.provider
-  return client.provider
-}
-
-function watchProvider<TProvider extends Provider>(
-  args: GetProviderArgs,
-  callback: any,
-) {
-  const client = getClient()
-  const unsubscribe = client.subscribe(
-    ({ provider }) => provider,
-    callback(client.getProvider({ chainId: args.chainId?.() })),
-  )
-  return unsubscribe
-}
-
-export function useProvider<TProvider extends Provider>(
-  props?: UseProviderArgs,
-) {
-  const [provider, setProvider] = createSignal<Provider>(getProvider())
+export function useProvider(props?: UseProviderArgs) {
+  const [provider, setProvider] = createSignal(getProvider())
 
   createEffect(() => {
-    const unsubscribe = watchProvider<TProvider>(
-      { chainId: props?.chainId },
-      (provider: Provider) => {
+    const unsubscribe = watchProvider(
+      { chainId: props?.chainId?.() },
+      (provider) => {
         setProvider(provider)
       },
     )
