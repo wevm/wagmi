87c3cfd0e4b7c89381a86f751303182161d537de
Tom Meagher <tom@meagher.co>
2023-03-20 16:40:21 -0400

    fix: type exports

--- packages/core/src/internal/index.ts
+++ packages/core/src/internal/index.ts
@@ -1,3 +1,9 @@
-export type { ContractsConfig, Contract } from '../types/contracts'
+export type {
+  CallOverrides,
+  Contract,
+  ContractsConfig,
+  Overrides,
+  PayableOverrides,
+} from '../types/contracts'
 
 export { debounce } from '../utils'
