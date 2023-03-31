1e16630961d8d387bde72399fd014cf349823d7e
moxey.eth <jakemoxey@gmail.com>
2023-03-31 11:09:29 +1100

    wip: update watchProvider, watchWebSocketProvider tests

--- packages/core/src/actions/providers/watchProvider.test.ts
+++ packages/core/src/actions/providers/watchProvider.test.ts
@@ -1,6 +1,7 @@
 import { describe, expect, it } from 'vitest'
 
 import { setupClient } from '../../../test'
+import { mainnet, optimism } from '../../chains'
 import { connect } from '../accounts/connect'
 import { disconnect } from '../accounts/disconnect'
 import type { GetProviderResult } from './getProvider'
@@ -13,15 +14,16 @@ describe('watchProvider', () => {
     const providers: GetProviderResult[] = []
     const unwatch = watchProvider({}, (provider) => providers.push(provider))
 
-    await connect({ connector: client.connectors[0]! })
+    await connect({ connector: client.connectors[0]!, chainId: mainnet.id })
     await disconnect()
-    await connect({ connector: client.connectors[0]! })
+    await connect({ connector: client.connectors[0]!, chainId: optimism.id })
+    await disconnect()
+    await connect({ connector: client.connectors[0]!, chainId: mainnet.id })
     unwatch()
 
     expect(providers).toMatchInlineSnapshot(`
       [
-        "<Provider network={1} />",
-        "<Provider network={1} />",
+        "<Provider network={10} />",
         "<Provider network={1} />",
       ]
     `)
--- packages/core/src/actions/providers/watchWebSocketProvider.test.ts
+++ packages/core/src/actions/providers/watchWebSocketProvider.test.ts
@@ -1,6 +1,7 @@
 import { describe, expect, it } from 'vitest'
 
 import { getWebSocketProvider, setupClient } from '../../../test'
+import { mainnet, optimism } from '../../chains'
 import { connect } from '../accounts/connect'
 import { disconnect } from '../accounts/disconnect'
 import type { GetWebSocketProviderResult } from './getWebSocketProvider'
@@ -9,25 +10,22 @@ import { watchWebSocketProvider } from './watchWebSocketProvider'
 describe('watchWebSocketProvider', () => {
   it('callback receives data', async () => {
     const client = setupClient({ webSocketProvider: getWebSocketProvider })
-    await client.webSocketProvider?.destroy()
 
     const providers: GetWebSocketProviderResult[] = []
     const unwatch = watchWebSocketProvider({}, (provider) =>
       providers.push(provider),
     )
 
-    await connect({ connector: client.connectors[0]! })
-    await client.webSocketProvider?.destroy()
+    await connect({ connector: client.connectors[0]!, chainId: mainnet.id })
     await disconnect()
-    await client.webSocketProvider?.destroy()
-    await connect({ connector: client.connectors[0]! })
-    await client.webSocketProvider?.destroy()
+    await connect({ connector: client.connectors[0]!, chainId: optimism.id })
+    await disconnect()
+    await connect({ connector: client.connectors[0]!, chainId: mainnet.id })
     unwatch()
 
     expect(providers).toMatchInlineSnapshot(`
       [
-        "<WebSocketProvider network={1} />",
-        "<WebSocketProvider network={1} />",
+        "<WebSocketProvider network={10} />",
         "<WebSocketProvider network={1} />",
       ]
     `)
