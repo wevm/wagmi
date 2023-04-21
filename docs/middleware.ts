4cdde44f3ec2766b5f35bed54c76da2d1ff2ec26
moxey.eth <jakemoxey@gmail.com>
2023-04-21 14:06:28 +1000

    docs: core actions

--- docs/middleware.ts
+++ docs/middleware.ts
@@ -7,6 +7,10 @@ const redirects: Record<string, string> = {
   '/docs/connectors/coinbase-wallet': '/docs/connectors/coinbaseWallet',
   '/docs/connectors/metamask': '/docs/connectors/metaMask',
   '/docs/connectors/walletconnect': '/docs/connectors/walletConnect',
+  '/docs/core/actions/fetchSigner': '/docs/core/actions/getWalletClient',
+  '/docs/core/actions/getProvider': '/docs/core/actions/getPublicClient',
+  '/docs/core/actions/getWebSocketProvider':
+    '/docs/core/actions/getWebSocketPublicClient',
   '/docs/create-wagmi': '/cli/create-wagmi',
   '/docs/migrating-to-030': '/docs/migrating-to-03', // Tweeted wrong link: https://twitter.com/awkweb/status/1518607780332122116
   '/docs/migrating-to-03': '/docs/migration-guide',
