79335b4c0fcd5e8152a2a1d28314c634db9d9cbf
Will Cory <willcory10@gmail.com>
2023-06-24 09:32:26 -0400

    fix: optimism goerli chainId (#2607)
    * updated op goreli chainId
    
    * changgeset
    
    ---------
    
    Co-authored-by: akash1412 <akashorasad2000@gmail.com>
    Co-authored-by: Will Cory <willcory@Wills-MacBook-Pro.local>

--- packages/cli/src/plugins/etherscan.ts
+++ packages/cli/src/plugins/etherscan.ts
@@ -7,7 +7,7 @@ const apiUrls = {
   [5]: 'https://api-goerli.etherscan.io/api',
   // Optimism
   [10]: 'https://api-optimistic.etherscan.io/api',
-  [69]: 'https://api-goerli-optimistic.etherscan.io/api',
+  [420]: 'https://api-goerli-optimistic.etherscan.io/api',
   // Polygon
   [137]: 'https://api.polygonscan.com/api',
   [80_001]: 'https://api-testnet.polygonscan.com/api',
