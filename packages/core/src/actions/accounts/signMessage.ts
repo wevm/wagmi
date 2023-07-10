1029585a5bbbfe567afcb4acaa5731d509431d11
Yuri Gii <abovezenith@gmail.com>
2023-07-10 18:27:45 +0200

    add viem sign message types

--- packages/core/src/actions/accounts/signMessage.ts
+++ packages/core/src/actions/accounts/signMessage.ts
@@ -1,11 +1,11 @@
-import type { SignMessageReturnType } from 'viem'
+import type { SignMessageReturnType, Hex, ByteArray } from 'viem'
 
 import { ConnectorNotFoundError } from '../../errors'
 import { getWalletClient } from '../viem'
 
 export type SignMessageArgs = {
   /** Message to sign with wallet */
-  message: string
+  message: string | { raw: Hex | ByteArray }
 }
 
 export type SignMessageResult = SignMessageReturnType
