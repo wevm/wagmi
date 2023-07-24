bc94d75cf3b61fe14d55f0cb2fb0c790b1c5a648
Yuri Gii <abovezenith@gmail.com>
2023-07-24 18:46:48 +0200

    Update packages/core/src/actions/accounts/signMessage.ts
    Co-authored-by: jxom  <jakemoxey@gmail.com>

--- packages/core/src/actions/accounts/signMessage.ts
+++ packages/core/src/actions/accounts/signMessage.ts
@@ -5,7 +5,7 @@ import { getWalletClient } from '../viem'
 
 export type SignMessageArgs = {
   /** Message to sign with wallet */
-  message: string | { raw: Hex | ByteArray }
+  message: SignMessageParameters['message']
 }
 
 export type SignMessageResult = SignMessageReturnType
