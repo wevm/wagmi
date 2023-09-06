bc7c60bc3f26735535c224d1fc9a1d5b7d764d2f
Tom Meagher <tom@meagher.co>
2023-09-06 17:44:37 -0400

    fix: test

--- packages/react/src/hooks/useSignMessage.test-d.ts
+++ packages/react/src/hooks/useSignMessage.test-d.ts
@@ -1,8 +1,5 @@
-import {
-  type Connector,
-  type SignMessageError,
-  type SignMessageParameters,
-} from '@wagmi/core'
+import { type SignMessageError } from '@wagmi/core'
+import type { SignMessageVariables } from '@wagmi/core/query'
 import { expectTypeOf, test } from 'vitest'
 
 import { useSignMessage } from './useSignMessage.js'
@@ -10,81 +7,52 @@ import { useSignMessage } from './useSignMessage.js'
 const message = 'hello world'
 const contextValue = { foo: 'bar' } as const
 
-type SignableMessage = SignMessageParameters['message']
-
 test('context', () => {
   const { context, data, error, signMessage, variables } = useSignMessage({
     onMutate(variables) {
-      expectTypeOf(variables).toEqualTypeOf<{
-        connector?: Connector | undefined
-        message: SignableMessage
-      }>()
+      expectTypeOf(variables).toEqualTypeOf<SignMessageVariables>()
       return contextValue
     },
     onError(error, variables, context) {
-      expectTypeOf(variables).toEqualTypeOf<{
-        connector?: Connector | undefined
-        message: SignableMessage
-      }>()
+      expectTypeOf(variables).toEqualTypeOf<SignMessageVariables>()
       expectTypeOf(error).toEqualTypeOf<SignMessageError>()
       expectTypeOf(context).toEqualTypeOf<typeof contextValue | undefined>()
     },
     onSuccess(data, variables, context) {
-      expectTypeOf(variables).toEqualTypeOf<{
-        connector?: Connector | undefined
-        message: SignableMessage
-      }>()
+      expectTypeOf(variables).toEqualTypeOf<SignMessageVariables>()
       expectTypeOf(data).toEqualTypeOf<`0x${string}`>()
       expectTypeOf(context).toEqualTypeOf<typeof contextValue | undefined>()
     },
     onSettled(data, error, variables, context) {
       expectTypeOf(data).toEqualTypeOf<`0x${string}` | undefined>()
       expectTypeOf(error).toEqualTypeOf<SignMessageError | null>()
-      expectTypeOf(variables).toEqualTypeOf<{
-        connector?: Connector | undefined
-        message: SignableMessage
-      }>()
+      expectTypeOf(variables).toEqualTypeOf<SignMessageVariables>()
       expectTypeOf(context).toEqualTypeOf<typeof contextValue | undefined>()
     },
   })
 
   expectTypeOf(data).toEqualTypeOf<`0x${string}` | undefined>()
   expectTypeOf(error).toEqualTypeOf<SignMessageError | null>()
-  expectTypeOf(variables).toEqualTypeOf<
-    | {
-        connector?: Connector | undefined
-        message: SignableMessage
-      }
-    | undefined
-  >()
+  expectTypeOf(variables).toEqualTypeOf<SignMessageVariables | undefined>()
   expectTypeOf(context).toEqualTypeOf<typeof contextValue | undefined>()
 
   signMessage(
     { message },
     {
       onError(error, variables, context) {
-        expectTypeOf(variables).toEqualTypeOf<{
-          connector?: Connector | undefined
-          message: SignableMessage
-        }>()
+        expectTypeOf(variables).toEqualTypeOf<SignMessageVariables>()
         expectTypeOf(error).toEqualTypeOf<SignMessageError>()
         expectTypeOf(context).toEqualTypeOf<typeof contextValue | undefined>()
       },
       onSuccess(data, variables, context) {
-        expectTypeOf(variables).toEqualTypeOf<{
-          connector?: Connector | undefined
-          message: SignableMessage
-        }>()
+        expectTypeOf(variables).toEqualTypeOf<SignMessageVariables>()
         expectTypeOf(data).toEqualTypeOf<`0x${string}`>()
         expectTypeOf(context).toEqualTypeOf<typeof contextValue>()
       },
       onSettled(data, error, variables, context) {
         expectTypeOf(data).toEqualTypeOf<`0x${string}` | undefined>()
         expectTypeOf(error).toEqualTypeOf<SignMessageError | null>()
-        expectTypeOf(variables).toEqualTypeOf<{
-          connector?: Connector | undefined
-          message: SignableMessage
-        }>()
+        expectTypeOf(variables).toEqualTypeOf<SignMessageVariables>()
         expectTypeOf(context).toEqualTypeOf<typeof contextValue | undefined>()
       },
     },
