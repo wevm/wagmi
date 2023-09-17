10318edec8abc71ac14d06423f133bf5bb105e18
Tom Meagher <tom@meagher.co>
2023-09-17 14:44:41 -0400

    chore: clean up

--- playgrounds/vite-core/vite.config.ts
+++ playgrounds/vite-core/vite.config.ts
@@ -1,17 +1,7 @@
-import path from 'path'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-
-const connectorsPath = '../../packages/connectors/src/exports'
-const corePath = '../../packages/core/src/exports'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@wagmi/connectors': path.resolve(__dirname, connectorsPath),
-      '@wagmi/core': path.resolve(__dirname, corePath),
-    },
-  },
 })
--- playgrounds/vite-react/vite.config.ts
+++ playgrounds/vite-react/vite.config.ts
@@ -1,19 +1,7 @@
-import path from 'path'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-
-const connectorsPath = '../../packages/connectors/src/exports'
-const corePath = '../../packages/core/src/exports'
-const reactPath = '../../packages/react/src/exports'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@wagmi/connectors': path.resolve(__dirname, connectorsPath),
-      '@wagmi/core': path.resolve(__dirname, corePath),
-      wagmi: path.resolve(__dirname, reactPath),
-    },
-  },
 })
