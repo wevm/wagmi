e00b82058685751637edfa9a6b2d196a12549fe7
jxom <jakemoxey@gmail.com>
2024-01-11 05:14:58 +1100

    fix: prelude gas estimation (#3407)
    * fix: prelude gas estimate in send transaction
    
    * chore: changeset
    
    * chore: update snap

--- packages/core/src/actions/sendTransaction.test.ts
+++ packages/core/src/actions/sendTransaction.test.ts
@@ -50,3 +50,34 @@ test('behavior: account does not exist on connector', async () => {
   `)
   await disconnect(config, { connector })
 })
+
+test('behavior: value exceeds balance', async () => {
+  await connect(config, { connector })
+  await expect(
+    sendTransaction(config, {
+      to: '0xd2135CfB216b74109775236E36d4b433F1DF507B',
+      value: parseEther('99999'),
+    }),
+  ).rejects.toThrowErrorMatchingInlineSnapshot(`
+    "The total cost (gas * gas fee + value) of executing this transaction exceeds the balance of the account.
+
+    This error could arise when the account does not have enough funds to:
+     - pay for the total gas fee,
+     - pay for the value to send.
+     
+    The cost of the transaction is calculated as \`gas * gas fee + value\`, where:
+     - \`gas\` is the amount of gas needed for transaction to execute,
+     - \`gas fee\` is the gas fee,
+     - \`value\` is the amount of ether to send to the recipient.
+     
+    Request Arguments:
+      from:   0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
+      to:     0xd2135CfB216b74109775236E36d4b433F1DF507B
+      value:  99999 ETH
+      gas:    21000
+
+    Details: Insufficient funds for gas * price + value
+    Version: viem@2.0.0"
+  `)
+  await disconnect(config, { connector })
+})
