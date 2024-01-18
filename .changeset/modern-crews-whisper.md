95adc8cd3771ba9308ed31666b85946e10c54f51
moxey.eth <jakemoxey@gmail.com>
2024-01-18 15:41:15 +1100

    fix: persist critical properties

---
+++ .changeset/modern-crews-whisper.md
@@ -0,0 +1,6 @@
+---
+"@wagmi/core": patch
+"wagmi": patch
+---
+
+Modified persist strategy to only store "critical" properties that are needed before hydration.
--- playgrounds/next/src/app/providers.tsx
+++ playgrounds/next/src/app/providers.tsx
@@ -2,15 +2,18 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { type ReactNode, useState } from 'react'
-import { WagmiProvider } from 'wagmi'
+import { State, WagmiProvider } from 'wagmi'
 
 import { config } from '../wagmi'
 
-export function Providers(props: { children: ReactNode }) {
+export function Providers(props: {
+  children: ReactNode
+  initialState?: State
+}) {
   const [queryClient] = useState(() => new QueryClient())
 
   return (
-    <WagmiProvider config={config}>
+    <WagmiProvider config={config} initialState={props.initialState}>
       <QueryClientProvider client={queryClient}>
         {props.children}
       </QueryClientProvider>
