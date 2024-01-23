5241b6cc5aadb142cb3213752282b3d8c386de98
awkweb <tom@meagher.co>
2024-01-22 23:31:32 -0500

    ci: fix open handles (#3499)
    ci: tweak

--- packages/test/src/globalSetup.ts
+++ packages/test/src/globalSetup.ts
@@ -1,34 +1,28 @@
-import { exec } from 'child_process'
 import { startProxy } from '@viem/anvil'
 
-import { type Chain, chain as chainLookup } from './chains.js'
+import { chain as chainLookup } from './chains.js'
 
 export default async function () {
   const promises = []
   for (const chain of Object.values(chainLookup)) {
-    promises.push(createProxy(chain))
+    promises.push(
+      startProxy({
+        port: chain.port,
+        host: '::',
+        options: {
+          chainId: chain.id,
+          forkUrl: chain.fork.url,
+          forkBlockNumber: chain.fork.blockNumber,
+          noMining: true,
+        },
+      }),
+    )
   }
-  await Promise.all(promises)
-}
+  const results = await Promise.all(promises)
 
-async function createProxy(chain: Chain) {
-  try {
-    await startProxy({
-      port: chain.port,
-      host: '::',
-      options: {
-        chainId: chain.id,
-        forkUrl: chain.fork.url,
-        forkBlockNumber: chain.fork.blockNumber,
-        noMining: true,
-      },
-    })
-  } catch (error) {
-    if ((error as Error).message.includes('EADDRINUSE')) {
-      console.error(
-        `createProxy: Chain ${chain.id} - ${(error as Error).message}`,
-      )
-      exec(`kill -9 $(lsof -t -i:${chain.port})`)
-    } else throw error
+  return () => {
+    for (const shutdown of results) {
+      shutdown()
+    }
   }
 }
