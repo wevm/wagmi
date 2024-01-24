3db6326c7f124b5d5b1eb92e59dcf1c8d42a54b4
Tom Meagher <tom@meagher.co>
2024-01-24 16:52:25 -0500

    chore: changeset

---
+++ .changeset/slimy-mayflies-mix.md
@@ -0,0 +1,5 @@
+---
+"@wagmi/cli": minor
+---
+
+Added resolution of TypeScript Wagmi CLI config to determine if TypeScript generated output is allowed.
--- packages/cli/src/utils/getIsUsingTypeScript.ts
+++ packages/cli/src/utils/getIsUsingTypeScript.ts
@@ -4,8 +4,14 @@ export async function getIsUsingTypeScript() {
   try {
     const cwd = process.cwd()
     const tsconfig = await findUp('tsconfig.json', { cwd })
-    const wagmiConfig = await findUp('wagmi.config.ts', { cwd })
-    return !!tsconfig || !!wagmiConfig
+    if (tsconfig) return true
+
+    const wagmiConfig = await findUp(['wagmi.config.ts', 'wagmi.config.mts'], {
+      cwd,
+    })
+    if (wagmiConfig) return true
+
+    return false
   } catch {
     return false
   }
