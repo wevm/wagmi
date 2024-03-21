7b122755bb63cd28b3f94ad7081a0ee6e682eb8f
Andreas Richter <708186+richtera@users.noreply.github.com>
2024-03-21 07:31:51 -0400

    fix: Minimize changes

--- packages/core/src/connectors/injected.ts
+++ packages/core/src/connectors/injected.ts
@@ -147,7 +147,7 @@ export function injected(parameters: InjectedParameters = {}) {
       if (!provider) throw new ProviderNotFoundError()
 
       let accounts: readonly Address[] | null = null
-      if (!isReconnecting) {
+      if (isReconnecting) {
         accounts = await this.getAccounts().catch(() => null)
         const isAuthorized = !!accounts?.length
         if (isAuthorized)
