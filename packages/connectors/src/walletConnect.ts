b052004c4baf510615b0724b84f1af802ee42155
Glitch <glitch.txs@gmail.com>
2024-04-03 20:52:36 -0300

    format

--- packages/connectors/src/walletConnect.ts
+++ packages/connectors/src/walletConnect.ts
@@ -268,7 +268,7 @@ export function walletConnect(parameters: WalletConnectParameters) {
             }),
           ),
         ])
-        
+
         const requestedChains = await this.getRequestedChainsIds()
         this.setRequestedChainsIds([...requestedChains, chainId])
         return chain
@@ -318,9 +318,9 @@ export function walletConnect(parameters: WalletConnectParameters) {
               throw new UserRejectedRequestError(
                 new Error('User rejected switch after adding network.'),
               )
-                
-          const requestedChains = await this.getRequestedChainsIds()
-          this.setRequestedChainsIds([...requestedChains, chainId])
+
+            const requestedChains = await this.getRequestedChainsIds()
+            this.setRequestedChainsIds([...requestedChains, chainId])
             return chain
           } catch (error) {
             throw new UserRejectedRequestError(error as Error)
