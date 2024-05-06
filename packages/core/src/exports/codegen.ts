30511e129c1a289a83ff50327a1d8064da3de5ab
Tom Meagher <tom@meagher.co>
2024-05-06 18:35:46 -0400

    chore: up knip

--- packages/core/src/exports/codegen.ts
+++ packages/core/src/exports/codegen.ts
@@ -1,7 +1,23 @@
-export { createSimulateContract } from '../actions/codegen/createSimulateContract.js'
+export {
+  type CreateSimulateContractParameters,
+  type CreateSimulateContractReturnType,
+  createSimulateContract,
+} from '../actions/codegen/createSimulateContract.js'
 
-export { createReadContract } from '../actions/codegen/createReadContract.js'
+export {
+  type CreateReadContractParameters,
+  type CreateReadContractReturnType,
+  createReadContract,
+} from '../actions/codegen/createReadContract.js'
 
-export { createWatchContractEvent } from '../actions/codegen/createWatchContractEvent.js'
+export {
+  type CreateWatchContractEventParameters,
+  type CreateWatchContractEventReturnType,
+  createWatchContractEvent,
+} from '../actions/codegen/createWatchContractEvent.js'
 
-export { createWriteContract } from '../actions/codegen/createWriteContract.js'
+export {
+  type CreateWriteContractParameters,
+  type CreateWriteContractReturnType,
+  createWriteContract,
+} from '../actions/codegen/createWriteContract.js'
