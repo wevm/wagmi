cff60e167029b12271566c17ba2ee5f024d396f4
moxey.eth <jakemoxey@gmail.com>
2024-05-09 21:20:32 +1000

    fix: type

--- packages/vue/src/types/ref.ts
+++ packages/vue/src/types/ref.ts
@@ -1,5 +1,6 @@
 // Credit: https://github.com/TanStack/query/blob/01ce023826b81e6c41e354f27691f65c9725af67/packages/vue-query/src/types.ts
 
+import type { Config } from '@wagmi/core'
 import type { MaybeRef, Ref, UnwrapRef } from 'vue'
 
 type Primitive = string | number | boolean | bigint | symbol | undefined | null
@@ -15,7 +16,7 @@ type UnwrapLeaf =
   | WeakSet<any>
 
 export type MaybeRefDeep<value> = MaybeRef<
-  value extends Function
+  value extends Function | Config
     ? value
     : value extends object
     ? {
