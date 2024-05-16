0157f36dba3ff2e00af448d177eb95c4b719ded9
Tom Meagher <tom@meagher.co>
2024-05-15 23:44:48 -0400

    chore: cov options

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     coverage: {
+      all: false,
       reporter: process.env.CI ? ['lcov'] : ['text', 'json', 'html'],
       exclude: [
         '**/dist/**',
