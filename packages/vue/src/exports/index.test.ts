f5c442d8d799d3fdf9e26d4f33ab11a1a05f7a16
Tom Meagher <tom@meagher.co>
2024-09-09 15:15:11 -0400

    chore: snaps

--- packages/vue/src/exports/index.test.ts
+++ packages/vue/src/exports/index.test.ts
@@ -39,6 +39,7 @@ test('exports', () => {
       "useTransaction",
       "useTransactionReceipt",
       "useWatchBlockNumber",
+      "useWatchContractEvent",
       "useWaitForTransactionReceipt",
       "useWriteContract",
       "createConfig",
