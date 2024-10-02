c19b1aad9f9a0e9f61b55fa1d842097c97d101de
Tom Meagher <tom@meagher.co>
2024-10-02 13:34:05 -0400

    chore: bump

--- playgrounds/vite-react/vite.config.ts
+++ playgrounds/vite-react/vite.config.ts
@@ -1,8 +1,7 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [solid(), react()],
+  plugins: [react()],
 })
