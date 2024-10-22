e563d7ae277899ae749fe3e765cdaa206b11fa1d
Tom Meagher <tom@meagher.co>
2024-10-21 20:56:00 -0400

    test: tweak

--- packages/core/src/actions/sendTransaction.test.ts
+++ packages/core/src/actions/sendTransaction.test.ts
@@ -31,7 +31,7 @@ test('behavior: connector not connected', async () => {
     sendTransaction(config, {
       connector: config.connectors[1],
       to: '0xd2135CfB216b74109775236E36d4b433F1DF507B',
-      value: parseEther('0.01'),
+      value: parseEther('0.0001'),
     }),
   ).rejects.toThrowErrorMatchingInlineSnapshot(`
     [ConnectorNotConnectedError: Connector not connected.
@@ -47,7 +47,7 @@ test('behavior: account does not exist on connector', async () => {
     sendTransaction(config, {
       account: '0xA0Cf798816D4b9b9866b5330EEa46a18382f251e',
       to: '0xd2135CfB216b74109775236E36d4b433F1DF507B',
-      value: parseEther('0.01'),
+      value: parseEther('0.0001'),
     }),
   ).rejects.toThrowErrorMatchingInlineSnapshot(`
     [ConnectorAccountNotFoundError: Account "0xA0Cf798816D4b9b9866b5330EEa46a18382f251e" not found for connector "Mock Connector".
