8f1844598ac16cacdc190cccef6808090fbd9695
ByteAtATime <byteatatime@proton.me>
2024-10-22 11:26:12 -0700

    feat: init svelte

---
+++ packages/svelte/src/app.d.ts
@@ -0,0 +1,13 @@
+// See https://svelte.dev/docs/kit/types#app
+// for information about these interfaces
+declare global {
+  namespace App {
+    // interface Error {}
+    // interface Locals {}
+    // interface PageData {}
+    // interface PageState {}
+    // interface Platform {}
+  }
+}
+
+export {}
