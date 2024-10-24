c9241f0d05376378e80edf7473221a4be2b18864
ByteAtATime <byteatatime@proton.me>
2024-10-24 12:30:03 -0700

    feat: create `readParameters` function

--- packages/svelte/src/lib/hooks/useConfig.svelte.ts
+++ packages/svelte/src/lib/hooks/useConfig.svelte.ts
@@ -2,10 +2,11 @@ import type { Config, ResolvedRegister } from '@wagmi/core'
 
 import { getWagmiConfig } from '$lib/context.js'
 import { WagmiProviderNotFoundError } from '$lib/errors.js'
-import type {
-  ConfigParameter,
-  RuneParameters,
-  RuneReturnType,
+import {
+  type ConfigParameter,
+  type RuneParameters,
+  type RuneReturnType,
+  readParameters,
 } from '$lib/types.js'
 
 export type UseConfigParameters<config extends Config = Config> =
@@ -18,7 +19,7 @@ export type UseConfigReturnType<config extends Config = Config> =
 export function useConfig<config extends Config = ResolvedRegister['config']>(
   parameters: UseConfigParameters<config> = {},
 ): UseConfigReturnType<config> {
-  const config = parameters.config ?? getWagmiConfig()
+  const config = $derived(readParameters(parameters).config ?? getWagmiConfig())
   if (!config) throw new WagmiProviderNotFoundError()
 
   return (() => config) as UseConfigReturnType<config>
--- packages/svelte/src/lib/types.ts
+++ packages/svelte/src/lib/types.ts
@@ -3,6 +3,9 @@ import type { Config } from '@wagmi/core'
 export type RuneParameters<T> = T | (() => T)
 export type RuneReturnType<T> = () => T
 
+export const readParameters = <T>(parameter: RuneParameters<T>): T =>
+  parameter instanceof Function ? parameter() : parameter
+
 export type ConfigParameter<config extends Config = Config> = {
   config?: Config | config | undefined
 }
