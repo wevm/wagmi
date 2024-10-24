a8894cf3a8bd6c14969640e17d1f9ca8269d3b95
ByteAtATime <byteatatime@proton.me>
2024-10-24 11:59:32 -0700

    refactor: use RuneReturnType instead of () => T

--- packages/svelte/src/lib/types.ts
+++ packages/svelte/src/lib/types.ts
@@ -1,5 +1,7 @@
 import type { Config } from '@wagmi/core'
 
+export type RuneReturnType<T> = () => T
+
 export type ConfigParameter<config extends Config = Config> = {
   config?: Config | config | undefined
 }
