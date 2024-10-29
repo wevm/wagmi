1694af760d499e4531047a3f980ed99ba2f17045
ByteAtATime <byteatatime@proton.me>
2024-10-29 12:43:46 -0700

    feat: add optimism chain support in +layout.svelte

--- packages/svelte/src/routes/+layout.svelte
+++ packages/svelte/src/routes/+layout.svelte
@@ -3,14 +3,15 @@ import './index.css'
 import WagmiProvider from '$lib/WagmiProvider.svelte'
 import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query'
 import { http, createConfig } from '@wagmi/core'
-import { mainnet, sepolia } from '@wagmi/core/chains'
+import { mainnet, optimism, sepolia } from '@wagmi/core/chains'
 import type { Snippet } from 'svelte'
 
 const config = createConfig({
-  chains: [mainnet, sepolia],
+  chains: [mainnet, sepolia, optimism],
   transports: {
     [mainnet.id]: http('https://rpc.ankr.com/eth'),
     [sepolia.id]: http(),
+    [optimism.id]: http(),
   },
 })
 
