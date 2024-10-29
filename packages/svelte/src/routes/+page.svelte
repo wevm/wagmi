c0bfcfffa09d83731147e89010ac64b24b85bd94
ByteAtATime <byteatatime@proton.me>
2024-10-29 09:41:00 -0700

    refactor: remove unused imports in +page.svelte

--- packages/svelte/src/routes/+page.svelte
+++ packages/svelte/src/routes/+page.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-import { useBlockNumber } from '$lib/hooks/useBlockNumber.svelte.js'
-import { useConfig } from '$lib/hooks/useConfig.svelte.js'
 import Account from './_components/Account.svelte'
 import Connect from './_components/Connect.svelte'
 import ReadContract from './_components/ReadContract.svelte'
