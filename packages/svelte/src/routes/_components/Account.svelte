072a84fc814ee780978f160649442f46cf824dd8
ByteAtATime <byteatatime@proton.me>
2024-10-29 09:40:47 -0700

    feat: add useAccount to playground

---
+++ packages/svelte/src/routes/_components/Account.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+import { useAccount } from '$lib/hooks/useAccount.svelte.js'
+
+const account = $derived.by(useAccount())
+// const { disconnect } = $derived.by(useDisconnect())
+// const { data: ensName } = $derived.by(useEnsName({
+//   address: account.address,
+// }))
+</script>
+
+  <div>
+    <h2>Account</h2>
+
+    <div>
+      account: {account.address}
+      <!-- {ensName} -->
+      <br />
+      chainId: {account.chainId}
+      <br />
+      status: {account.status}
+    </div>
+
+    <!-- {#if account.status !== 'disconnected'}
+      <button type="button" onClick={() => disconnect()}>
+        Disconnect
+      </button>
+    {/if} -->
+  </div>
