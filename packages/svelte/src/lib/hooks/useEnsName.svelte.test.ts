93eca298898fbd4788cb6c8e0b2188304cd0a0da
ByteAtATime <byteatatime@proton.me>
2024-10-31 12:29:34 -0700

    feat: useEnsName hook

---
+++ packages/svelte/src/lib/hooks/useEnsName.svelte.test.ts
@@ -0,0 +1,52 @@
+import { expect, test } from 'vitest'
+import { testHook } from './test.svelte.js'
+import { useEnsName } from './useEnsName.svelte'
+
+test(
+  'default',
+  testHook(async () => {
+    const result = $derived.by(
+      useEnsName(() => ({
+        address: '0xd2135CfB216b74109775236E36d4b433F1DF507B',
+      })),
+    )
+
+    await expect.poll(() => result.isSuccess).toBeTruthy()
+
+    expect(result).toMatchInlineSnapshot(`
+      {
+        "data": "wevm.eth",
+        "dataUpdatedAt": 1675209600000,
+        "error": null,
+        "errorUpdateCount": 0,
+        "errorUpdatedAt": 0,
+        "failureCount": 0,
+        "failureReason": null,
+        "fetchStatus": "idle",
+        "isError": false,
+        "isFetched": true,
+        "isFetchedAfterMount": true,
+        "isFetching": false,
+        "isInitialLoading": false,
+        "isLoading": false,
+        "isLoadingError": false,
+        "isPaused": false,
+        "isPending": false,
+        "isPlaceholderData": false,
+        "isRefetchError": false,
+        "isRefetching": false,
+        "isStale": true,
+        "isSuccess": true,
+        "queryKey": [
+          "ensName",
+          {
+            "address": "0xd2135CfB216b74109775236E36d4b433F1DF507B",
+            "chainId": 1,
+          },
+        ],
+        "refetch": [Function],
+        "status": "success",
+      }
+    `)
+  }),
+)
