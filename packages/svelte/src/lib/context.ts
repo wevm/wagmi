aa076131b46f537766f38be9f96ed4951b3835db
ByteAtATime <byteatatime@proton.me>
2024-10-31 12:26:52 -0700

    fix: don't throw early if missing Wagmi context

--- packages/svelte/src/lib/context.ts
+++ packages/svelte/src/lib/context.ts
@@ -6,11 +6,6 @@ const WAGMI_CONFIG_KEY = Symbol('WAGMI_CONFIG')
 
 export const getWagmiConfig = (): ContextConfig => {
   const client = getContext<ContextConfig>(WAGMI_CONFIG_KEY)
-  if (!client) {
-    throw new Error(
-      'No Wagmi context was found in Svelte context. Did you forget to wrap your component with WagmiProvider?',
-    )
-  }
 
   return client
 }
