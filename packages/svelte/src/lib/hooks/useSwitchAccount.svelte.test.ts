ca097fb33661403ae9a12ce70527efef3ebd8998
ByteAtATime <byteatatime@proton.me>
2024-10-31 12:38:29 -0700

    feat; useSwitchAccount hook

---
+++ packages/svelte/src/lib/hooks/useSwitchAccount.svelte.test.ts
@@ -0,0 +1,40 @@
+import { connect, disconnect } from '@wagmi/core'
+import { config } from '@wagmi/test'
+import { expect, test } from 'vitest'
+import { testHook } from './test.svelte.js'
+import { useAccount } from './useAccount.svelte.js'
+import { useSwitchAccount } from './useSwitchAccount.svelte.js'
+
+const connector1 = config.connectors[0]!
+const connector2 = config.connectors[1]!
+
+test(
+  'default',
+  testHook(async () => {
+    await connect(config, { connector: connector2 })
+    await connect(config, { connector: connector1 })
+
+    const account = $derived.by(useAccount())
+    const switchAccount = $derived.by(useSwitchAccount())
+
+    const address1 = account.address
+    expect(address1).toBeDefined()
+
+    switchAccount.switchAccount({ connector: connector2 })
+    await expect.poll(() => switchAccount.isSuccess).toBeTruthy()
+
+    const address2 = account.address
+    expect(address2).toBeDefined()
+    expect(address1).not.toBe(address2)
+
+    switchAccount.switchAccount({ connector: connector1 })
+    await expect.poll(() => switchAccount.isSuccess).toBeTruthy()
+
+    const address3 = account.address
+    expect(address3).toBeDefined()
+    expect(address1).toBe(address3)
+
+    await disconnect(config, { connector: connector1 })
+    await disconnect(config, { connector: connector2 })
+  }),
+)
