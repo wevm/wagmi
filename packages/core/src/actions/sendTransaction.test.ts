5f8ee5fb682e1cbd90cba83fe79f90a8150e0d28
Tom Meagher <tom@meagher.co>
2024-11-07 13:11:39 -0500

    test: tweaks

--- packages/core/src/actions/sendTransaction.test.ts
+++ packages/core/src/actions/sendTransaction.test.ts
@@ -99,7 +99,7 @@ test('behavior: local account', async () => {
     sendTransaction(config, {
       account,
       to: '0xd2135CfB216b74109775236E36d4b433F1DF507B',
-      value: parseEther('0.0001'),
+      value: parseEther('0.000001'),
     }),
   ).resolves.toMatch(transactionHashRegex)
 })
