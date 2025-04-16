c64d581b1da4834fd043654d445912fae7088396
Alexandre Jouandin <alexandre@jouand.in>
2025-04-16 15:15:57 +0000

    fix(cli): in Sourcify plugin, only fetch ABI data rather than all of metadata

--- packages/cli/src/plugins/sourcify.ts
+++ packages/cli/src/plugins/sourcify.ts
@@ -29,20 +29,7 @@ export type SourcifyConfig<chainId extends number> = {
 }
 
 const SourcifyResponse = z.object({
-  metadata: z.object({
-    compiler: z.object({
-      version: z.string(),
-    }),
-    language: z.string(),
-    output: z.object({
-      abi: AbiSchema,
-      devdoc: z.any(),
-      userdoc: z.any(),
-    }),
-    settings: z.any(),
-    sources: z.any(),
-    version: z.number(),
-  })
+  abi: AbiSchema,
 })
 
 /** Fetches contract ABIs from Sourcify. */
@@ -69,8 +56,8 @@ export function sourcify<chainId extends ChainId>(
       if (!parsed.success)
         throw fromZodError(parsed.error, { prefix: 'Invalid response' })
 
-      if (parsed.data.metadata.output.abi)
-        return parsed.data.metadata.output.abi as ContractConfig['abi']
+      if (parsed.data.abi)
+        return parsed.data.abi as ContractConfig['abi']
       throw new Error('contract not found')
     },
     request({ address }) {
@@ -86,7 +73,7 @@ export function sourcify<chainId extends ChainId>(
         )
 
       return {
-        url: `https://sourcify.dev/server/v2/contract/${chainId}/${contractAddress}/?fields=metadata`,
+        url: `https://sourcify.dev/server/v2/contract/${chainId}/${contractAddress}/?fields=abi`,
       }
     },
   })
