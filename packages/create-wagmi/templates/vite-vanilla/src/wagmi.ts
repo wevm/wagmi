9732328918681df3602d2640dbcaae5af1507b60
Tom Meagher <tom@meagher.co>
2025-11-10 17:59:09 -0500

    chore: up create templates

--- packages/create-wagmi/templates/vite-vanilla/src/wagmi.ts
+++ packages/create-wagmi/templates/vite-vanilla/src/wagmi.ts
@@ -1,14 +1,8 @@
-import { baseAccount, injected, walletConnect } from '@wagmi/connectors'
 import { createConfig, http } from '@wagmi/core'
 import { mainnet, sepolia } from '@wagmi/core/chains'
 
 export const config = createConfig({
   chains: [mainnet, sepolia],
-  connectors: [
-    injected(),
-    baseAccount(),
-    walletConnect({ projectId: import.meta.env.VITE_WC_PROJECT_ID }),
-  ],
   transports: {
     [mainnet.id]: http(),
     [sepolia.id]: http(),
--- packages/create-wagmi/templates/vite-vue/src/wagmi.ts
+++ packages/create-wagmi/templates/vite-vue/src/wagmi.ts
@@ -1,15 +1,8 @@
 import { createConfig, createStorage, http } from '@wagmi/vue'
 import { mainnet, optimism, sepolia } from '@wagmi/vue/chains'
-import { baseAccount, walletConnect } from '@wagmi/vue/connectors'
 
 export const config = createConfig({
   chains: [mainnet, sepolia, optimism],
-  connectors: [
-    walletConnect({
-      projectId: import.meta.env.VITE_WC_PROJECT_ID,
-    }),
-    baseAccount({ appName: 'Vite Vue Playground', darkMode: true }),
-  ],
   storage: createStorage({ storage: localStorage, key: 'vite-vue' }),
   transports: {
     [mainnet.id]: http(),
