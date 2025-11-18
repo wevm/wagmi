1c216564eb72f265a33cc048d2a2d08c2d3a75ee
Tom Meagher <tom@meagher.co>
2025-11-18 10:44:10 -0500

    chore: tweaks

--- packages/connectors/src/porto.ts
+++ packages/connectors/src/porto.ts
@@ -4,7 +4,7 @@ import {
   createConnector,
 } from '@wagmi/core'
 import type { ExactPartial } from '@wagmi/core/internal'
-import { type Porto, RpcSchema } from 'porto'
+import type { Porto, RpcSchema } from 'porto'
 import { z } from 'porto/internal'
 import {
   type Address,
@@ -89,6 +89,7 @@ export function porto(parameters: PortoParameters = {}) {
 
         try {
           if (!accounts?.length && !isReconnecting) {
+            const { RpcSchema } = await import('porto')
             const res = await provider.request({
               method: 'wallet_connect',
               params: [
