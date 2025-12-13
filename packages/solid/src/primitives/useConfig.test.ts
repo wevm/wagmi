f170c63cd4600447dc6038468a07da602e86034a
Wonhee Lee <2wheeh@gmail.com>
2025-12-13 14:40:10 +0900

    test: add useConfig test for not provider error

--- packages/solid/src/primitives/useConfig.test.ts
+++ packages/solid/src/primitives/useConfig.test.ts
@@ -1,9 +1,34 @@
 import { config } from '@wagmi/test'
-import { expect, test } from 'vitest'
-
+import { createWrapper, renderPrimitive } from '@wagmi/test/solid'
+import { createComponent } from 'solid-js'
+import { template } from 'solid-js/web'
+import { expect, test, vi } from 'vitest'
 import { useConfig } from './useConfig.js'
 
 test('default', () => {
-  const result = useConfig(() => ({ config }))
+  const { result } = renderPrimitive(useConfig(() => ({ config })))
   expect(result).toBeDefined()
+})
+
+test('behavior: throws when not inside Provider', async () => {
+  vi.spyOn(console, 'error').mockImplementation(() => {})
+
+  try {
+    renderPrimitive(
+      useConfig(() => ({})),
+      {
+        wrapper: createWrapper(
+          (props) => createComponent(() => template('div')(), props),
+          undefined,
+        ),
+      },
+    )
+  } catch (error) {
+    expect(error).toMatchInlineSnapshot(`
+      [WagmiProviderNotFoundError: \`useConfig\` must be used within \`WagmiProvider\`.
+
+      Docs: https://wagmi.sh/solid/api/WagmiProvider.html
+      Version: @wagmi/solid@x.y.z]
+    `)
+  }
 })
