ea33b71dc4575305bd67c8e69680dd2d8583b4e4
Wonhee Lee <2wheeh@gmail.com>
2025-12-17 13:52:14 +0900

    feat: useReconnect

---
+++ packages/solid/src/primitives/useReconnect.test-d.ts
@@ -0,0 +1,155 @@
+import type {
+  Connector,
+  CreateConnectorFn,
+  ReconnectErrorType,
+} from '@wagmi/core'
+import { config } from '@wagmi/test'
+import type { Address } from 'viem'
+import { expectTypeOf, test } from 'vitest'
+import { useReconnect } from './useReconnect.js'
+
+const connectors = [config.connectors[0]!]
+const contextValue = { foo: 'bar' } as const
+
+test('context', () => {
+  const reconnect = useReconnect(() => ({
+    mutation: {
+      onMutate(variables) {
+        expectTypeOf(variables).toEqualTypeOf<
+          | {
+              connectors?:
+                | readonly (CreateConnectorFn | Connector)[]
+                | undefined
+            }
+          | undefined
+        >()
+        return contextValue
+      },
+      onError(error, variables, context) {
+        expectTypeOf(variables).toEqualTypeOf<
+          | {
+              connectors?:
+                | readonly (CreateConnectorFn | Connector)[]
+                | undefined
+            }
+          | undefined
+        >()
+        expectTypeOf(error).toEqualTypeOf<ReconnectErrorType>()
+        expectTypeOf(context).toEqualTypeOf<typeof contextValue | undefined>()
+      },
+      onSuccess(data, variables, context) {
+        expectTypeOf(variables).toEqualTypeOf<
+          | {
+              connectors?:
+                | readonly (CreateConnectorFn | Connector)[]
+                | undefined
+            }
+          | undefined
+        >()
+        expectTypeOf(data).toEqualTypeOf<
+          {
+            accounts: readonly [Address, ...Address[]]
+            chainId: number
+            connector: Connector
+          }[]
+        >()
+        expectTypeOf(context).toEqualTypeOf<typeof contextValue>()
+      },
+      onSettled(data, error, variables, context) {
+        expectTypeOf(data).toEqualTypeOf<
+          | {
+              accounts: readonly [Address, ...Address[]]
+              chainId: number
+              connector: Connector
+            }[]
+          | undefined
+        >()
+        expectTypeOf(error).toEqualTypeOf<ReconnectErrorType | null>()
+        expectTypeOf(variables).toEqualTypeOf<
+          | {
+              connectors?:
+                | readonly (CreateConnectorFn | Connector)[]
+                | undefined
+            }
+          | undefined
+        >()
+        expectTypeOf(context).toEqualTypeOf<typeof contextValue | undefined>()
+      },
+    },
+  }))
+
+  expectTypeOf(reconnect.data).toEqualTypeOf<
+    | {
+        accounts: readonly [Address, ...Address[]]
+        chainId: number
+        connector: Connector
+      }[]
+    | undefined
+  >()
+  expectTypeOf(reconnect.error).toEqualTypeOf<ReconnectErrorType | null>()
+  expectTypeOf(reconnect.variables).toEqualTypeOf<
+    | {
+        connectors?: readonly (CreateConnectorFn | Connector)[] | undefined
+      }
+    | undefined
+  >()
+  expectTypeOf(reconnect.context).toEqualTypeOf<
+    typeof contextValue | undefined
+  >()
+
+  reconnect.mutate(
+    { connectors },
+    {
+      onError(error, variables, context) {
+        expectTypeOf(variables).toEqualTypeOf<
+          | {
+              connectors?:
+                | readonly (CreateConnectorFn | Connector)[]
+                | undefined
+            }
+          | undefined
+        >()
+        expectTypeOf(error).toEqualTypeOf<ReconnectErrorType>()
+        expectTypeOf(context).toEqualTypeOf<typeof contextValue | undefined>()
+      },
+      onSuccess(data, variables, context) {
+        expectTypeOf(variables).toEqualTypeOf<
+          | {
+              connectors?:
+                | readonly (CreateConnectorFn | Connector)[]
+                | undefined
+            }
+          | undefined
+        >()
+        expectTypeOf(data).toEqualTypeOf<
+          {
+            accounts: readonly [Address, ...Address[]]
+            chainId: number
+            connector: Connector
+          }[]
+        >()
+        expectTypeOf(context).toEqualTypeOf<typeof contextValue>()
+      },
+      onSettled(data, error, variables, context) {
+        expectTypeOf(data).toEqualTypeOf<
+          | {
+              accounts: readonly [Address, ...Address[]]
+              chainId: number
+              connector: Connector
+            }[]
+          | undefined
+        >()
+        expectTypeOf(error).toEqualTypeOf<ReconnectErrorType | null>()
+        expectTypeOf(variables).toEqualTypeOf<
+          | {
+              connectors?:
+                | readonly (CreateConnectorFn | Connector)[]
+                | undefined
+            }
+          | undefined
+        >()
+        expectTypeOf(context).toEqualTypeOf<typeof contextValue | undefined>()
+      },
+    },
+  )
+})
---
+++ packages/solid/src/primitives/useReconnect.ts
@@ -0,0 +1,71 @@
+import { useMutation } from '@tanstack/solid-query'
+import type { Connector, ReconnectErrorType } from '@wagmi/core'
+import type { Compute } from '@wagmi/core/internal'
+import {
+  type ReconnectData,
+  type ReconnectMutate,
+  type ReconnectMutateAsync,
+  type ReconnectVariables,
+  reconnectMutationOptions,
+} from '@wagmi/core/query'
+import { type Accessor, createMemo, mergeProps } from 'solid-js'
+
+import type { ConfigParameter } from '../types/properties.js'
+import type {
+  SolidMutationParameters,
+  UseMutationReturnType,
+} from '../utils/query.js'
+import { useConfig } from './useConfig.js'
+
+export type SolidReconnectParameters<context = unknown> = Compute<
+  ConfigParameter & {
+    mutation?:
+      | SolidMutationParameters<
+          ReconnectData,
+          ReconnectErrorType,
+          ReconnectVariables,
+          context
+        >
+      | undefined
+  }
+>
+
+export type UseReconnectParameters<context = unknown> = Accessor<
+  SolidReconnectParameters<context>
+>
+
+export type UseReconnectReturnType<context = unknown> = Compute<
+  UseMutationReturnType<
+    ReconnectData,
+    ReconnectErrorType,
+    ReconnectVariables,
+    context
+  > & {
+    connectors: readonly Connector[]
+    mutate: ReconnectMutate<context>
+    mutateAsync: ReconnectMutateAsync<context>
+    /** @deprecated use `mutate` instead */
+    reconnect: ReconnectMutate<context>
+    /** @deprecated use `mutateAsync` instead */
+    reconnectAsync: ReconnectMutateAsync<context>
+  }
+>
+
+/** https://wagmi.sh/solid/api/primitives/useReconnect */
+export function useReconnect<context = unknown>(
+  parameters: UseReconnectParameters<context> = () => ({}),
+): UseReconnectReturnType<context> {
+  const config = useConfig(parameters)
+
+  const mutationOptions = createMemo(() => reconnectMutationOptions(config()))
+  const mutation = useMutation(() => ({
+    ...parameters().mutation,
+    ...mutationOptions(),
+  }))
+
+  return mergeProps(mutation, {
+    connectors: config().connectors,
+    reconnect: mutation.mutate,
+    reconnectAsync: mutation.mutateAsync,
+  }) as UseReconnectReturnType<context>
+}
