f70d3024ac5a5e86b38a21923df564d368405a24
jxom <7336481+jxom@users.noreply.github.com>
2026-01-09 20:45:44 +1100

    chore: tweak

--- packages/connectors/src/exports/index.ts
+++ packages/connectors/src/exports/index.ts
@@ -1,21 +1,10 @@
-// biome-ignore lint/performance/noBarrelFile: entrypoint module
-export {
-  type InjectedParameters,
-  injected,
-  type MockParameters,
-  mock,
-} from '@wagmi/core'
-export { type BaseAccountParameters, baseAccount } from '../baseAccount.js'
-export {
-  type CoinbaseWalletParameters,
-  coinbaseWallet,
-} from '../coinbaseWallet.js'
-export { type GeminiParameters, gemini } from '../gemini.js'
-export { type MetaMaskParameters, metaMask } from '../metaMask.js'
-export { type PortoParameters, porto } from '../porto.js'
-export { type SafeParameters, safe } from '../safe.js'
-export { version } from '../version.js'
-export {
-  type WalletConnectParameters,
-  walletConnect,
-} from '../walletConnect.js'
+export { type InjectedParameters, injected, type MockParameters, mock, } from '@wagmi/core';
+export { type BaseAccountParameters, baseAccount } from '../baseAccount.js';
+export { type CoinbaseWalletParameters, coinbaseWallet, } from '../coinbaseWallet.js';
+export { type GeminiParameters, gemini } from '../gemini.js';
+export { type MetaMaskParameters, metaMask } from '../metaMask.js';
+export { type PortoParameters, porto } from '../porto.js';
+export { type SafeParameters, safe } from '../safe.js';
+export { version } from '../version.js';
+export { type WalletConnectParameters, walletConnect, } from '../walletConnect.js';
+//# sourceMappingURL=index.d.ts.map
