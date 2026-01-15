119c5cdb6e695ec72e54dd907f3c794ac6e4ec51
jxom <7336481+jxom@users.noreply.github.com>
2026-01-15 23:55:20 +0000

    chore: up tests

--- packages/core/src/actions/sendTransaction.test.ts
+++ packages/core/src/actions/sendTransaction.test.ts
@@ -87,7 +87,7 @@ test('behavior: value exceeds balance', async () => {
       value:  99999 ETH
 
     Details: Insufficient funds for gas * price + value
-    Version: viem@2.44.0]
+    Version: viem@2.44.4]
   `)
   await disconnect(config, { connector })
 })
--- packages/core/src/actions/sendTransactionSync.test.ts
+++ packages/core/src/actions/sendTransactionSync.test.ts
@@ -3,6 +3,7 @@ import { parseEther } from 'viem'
 import { beforeEach, expect, test } from 'vitest'
 import { connect } from './connect.js'
 import { disconnect } from './disconnect.js'
+import { sendTransaction } from './sendTransaction.js'
 import { sendTransactionSync } from './sendTransactionSync.js'
 
 const connector = config.connectors[0]!
@@ -86,12 +87,13 @@ test('behavior: value exceeds balance', async () => {
      - \`value\` is the amount of ether to send to the recipient.
      
     Request Arguments:
+      chain:  Ethereum (id: 1)
       from:   0x95132632579b073D12a6673e18Ab05777a6B86f8
       to:     0xd2135CfB216b74109775236E36d4b433F1DF507B
       value:  99999 ETH
 
     Details: Insufficient funds for gas * price + value
-    Version: viem@2.44.0]
+    Version: viem@2.44.4]
   `)
   await disconnect(config, { connector })
 })
