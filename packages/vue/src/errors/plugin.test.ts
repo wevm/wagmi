7db83abc58d04ceb0ae0df4333e4106513350757
Tom Meagher <tom@meagher.co>
2026-01-21 13:09:43 -0500

    chore: snaps

--- packages/vue/src/errors/plugin.test.ts
+++ packages/vue/src/errors/plugin.test.ts
@@ -9,7 +9,7 @@ test('WagmiPluginNotFoundError', () => {
   expect(new WagmiPluginNotFoundError()).toMatchInlineSnapshot(`
     [WagmiPluginNotFoundError: No \`config\` found in Vue context, use \`WagmiPlugin\` to properly initialize the library.
 
-    Docs: https://wagmi.sh/vue/api/TODO.html
+    Docs: https://wagmi.sh/vue/api/Nuxt.html
     Version: @wagmi/vue@x.y.z]
   `)
 })
@@ -18,7 +18,7 @@ test('WagmiInjectionContextError', () => {
   expect(new WagmiInjectionContextError()).toMatchInlineSnapshot(`
     [WagmiInjectionContextError: Wagmi composables can only be used inside \`setup()\` function or functions that support injection context.
 
-    Docs: https://wagmi.sh/vue/api/TODO.html
+    Docs: https://wagmi.sh/vue/api/Nuxt.html
     Version: @wagmi/vue@x.y.z]
   `)
 })
