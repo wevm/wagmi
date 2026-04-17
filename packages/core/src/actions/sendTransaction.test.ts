b2b794ce98941cbc1567c66452131e115789628c
tmm <tmm@tmm.dev>
2026-04-17 15:09:32 -0400

    chore: snaps

--- packages/core/src/actions/sendTransaction.test.ts
+++ packages/core/src/actions/sendTransaction.test.ts
@@ -88,7 +88,7 @@ test('behavior: value exceeds balance', async () => {
       value:  99999 ETH
 
     Details: Insufficient funds for gas * price + value
-    Version: viem@2.47.5]
+    Version: viem@2.48.0]
   `)
   await disconnect(config, { connector })
 })
