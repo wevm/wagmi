8aeffd61ec8509131775eff1b2de9b27601ad6f1
Alex Mendonca <alex.mendonca@consensys.net>
2026-04-21 10:18:15 +0100

    refactor(connectors)!: align return types

--- packages/connectors/src/metaMask.ts
+++ packages/connectors/src/metaMask.ts
@@ -80,16 +80,20 @@ export function metaMask(parameters: MetaMaskParameters = {}) {
           const chainIds = config.chains.map((chain) => numberToHex(chain.id))
           if (parameters.connectAndSign || parameters.connectWith) {
             if (parameters.connectAndSign)
-              signResponse = await instance.connectAndSign({
-                chainIds,
-                message: parameters.connectAndSign,
-              })
+              signResponse = (
+                await instance.connectAndSign({
+                  chainIds,
+                  message: parameters.connectAndSign,
+                })
+              ).signature
             else if (parameters.connectWith)
-              connectWithResponse = await instance.connectWith({
-                chainIds,
-                method: parameters.connectWith.method,
-                params: parameters.connectWith.params,
-              })
+              connectWithResponse = (
+                await instance.connectWith({
+                  chainIds,
+                  method: parameters.connectWith.method,
+                  params: parameters.connectWith.params,
+                })
+              ).result
 
             accounts = await this.getAccounts()
           } else {
@@ -111,13 +115,13 @@ export function metaMask(parameters: MetaMaskParameters = {}) {
           provider.emit('connectAndSign', {
             accounts,
             chainId: numberToHex(currentChainId),
-            signResponse,
+            signature: signResponse,
           })
         else if (connectWithResponse)
           provider.emit('connectWith', {
             accounts,
             chainId: numberToHex(currentChainId),
-            connectWithResponse,
+            result: connectWithResponse,
           })
 
         return {
