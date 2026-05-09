16f1d265552b56e19b2f0c29c28c86ba434764d0
MyRxWallet <developer@myrxwallet.io>
2026-05-09 23:18:14 +0000

    Export myrx chain (MYRX-MAINNET, chain ID 8472) from viem/chains

--- packages/core/src/exports/chains.ts
+++ packages/core/src/exports/chains.ts
@@ -5,3 +5,4 @@
 // biome-ignore lint/performance/noBarrelFile: entrypoint module
 // biome-ignore lint/performance/noReExportAll: entrypoint module
 export * from 'viem/chains'
+export { myrx } from "viem/chains"
