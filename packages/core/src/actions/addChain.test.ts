509e91a7fea67e07ac1244ac5417599f71d40939
tmm <tmm@tmm.dev>
2026-08-12 13:07:01 -0400

    test: improve add chain coverage

--- packages/core/src/actions/addChain.test.ts
+++ packages/core/src/actions/addChain.test.ts
@@ -1,7 +1,8 @@
-import { config } from '@wagmi/test'
+import { accounts, config } from '@wagmi/test'
 import { avalanche } from 'viem/chains'
-import { expect, test } from 'vitest'
+import { expect, test, vi } from 'vitest'
 
+import { mock } from '../connectors/mock.js'
 import { addChain } from './addChain.js'
 import { connect } from './connect.js'
 import { disconnect } from './disconnect.js'
@@ -10,14 +11,60 @@ const connector = config.connectors[0]!
 
 test('default', async () => {
   await connect(config, { connector })
-  await expect(addChain(config, { chain: avalanche })).resolves.toBeUndefined()
-  await disconnect(config, { connector })
+  try {
+    await expect(
+      addChain(config, { chain: avalanche }),
+    ).resolves.toBeUndefined()
+  } finally {
+    await disconnect(config, { connector })
+  }
 })
 
-test('no block explorer', async () => {
-  await connect(config, { connector })
-  await addChain(config, {
-    chain: { ...avalanche, blockExplorers: undefined },
-  })
-  await disconnect(config, { connector })
+test('parameters: connector and chain', async () => {
+  const connector_ = config._internal.connectors.setup(mock({ accounts }))
+  const provider = await connector_.getProvider()
+  const request = vi.spyOn(provider, 'request')
+  vi.spyOn(connector_, 'getProvider').mockResolvedValue(provider)
+  await connect(config, { connector: connector_ })
+  request.mockClear()
+
+  try {
+    await addChain(config, {
+      chain: { ...avalanche, blockExplorers: undefined },
+      connector: connector_,
+    })
+    expect(request).toHaveBeenCalledWith(
+      {
+        method: 'wallet_addEthereumChain',
+        params: [
+          expect.objectContaining({
+            blockExplorerUrls: undefined,
+            chainId: '0xa86a',
+            chainName: 'Avalanche',
+          }),
+        ],
+      },
+      undefined,
+    )
+  } finally {
+    await disconnect(config, { connector: connector_ })
+  }
+})
+
+test('behavior: connector error', async () => {
+  const error = new Error('Failed to add chain.')
+  const connector_ = config._internal.connectors.setup(
+    mock({ accounts, features: { addChainError: error } }),
+  )
+  await connect(config, { connector: connector_ })
+  try {
+    await expect(
+      addChain(config, { chain: avalanche, connector: connector_ }),
+    ).rejects.toMatchObject({
+      details: error.message,
+      name: 'UnknownRpcError',
+    })
+  } finally {
+    await disconnect(config, { connector: connector_ })
+  }
 })
--- packages/core/src/query/addChain.test.ts
+++ packages/core/src/query/addChain.test.ts
@@ -1,7 +1,12 @@
 import { config } from '@wagmi/test'
+import { avalanche } from 'viem/chains'
 import { expect, test } from 'vitest'
 
+import { connect } from '../actions/connect.js'
+import { disconnect } from '../actions/disconnect.js'
 import { addChainMutationOptions } from './addChain.js'
+
+const connector = config.connectors[0]!
 
 test('default', () => {
   expect(addChainMutationOptions(config)).toMatchInlineSnapshot(`
@@ -12,4 +17,16 @@ test('default', () => {
       ],
     }
   `)
+})
+
+test('mutationFn', async () => {
+  await connect(config, { connector })
+  try {
+    const options = addChainMutationOptions(config)
+    await expect(
+      options.mutationFn?.({ chain: avalanche, connector }),
+    ).resolves.toBeUndefined()
+  } finally {
+    await disconnect(config, { connector })
+  }
 })
